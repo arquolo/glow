2a1b51ce8c1a88613c62fefb0265c7c71d30ef5a
Paul M <arquolo@gmail.com>
2019-04-17 22:30:55 +0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setuptools.setup(
     name='ort',
-    version='0.0.3',
+    version='0.0.4',
     url="https://github.com/arquolo/ort",
     author='Paul Maevskikh',
     author_email='arquolo@gmail.com',
