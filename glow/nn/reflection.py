9ebc4ff4e1dd4c5b291922d02071fbc1236e11b9
Paul M <arquolo@gmail.com>
2019-08-01 21:22:42 +0500

    Improve nn.reflection
    Improve nn.reflection

--- glow/nn/reflection.py
+++ glow/nn/reflection.py
@@ -1,9 +1,13 @@
 __all__ = 'get_gpu_state', 'plot_model'
 
+import functools
 import os
+from contextlib import ExitStack
 
+import graphviz
 import torch
-from graphviz import Digraph
+
+from ..core import decimate
 
 
 def get_gpu_state():
@@ -24,11 +28,30 @@ def get_gpu_state():
     devices = (nvmlDeviceGetHandleByIndex(i) for i in ids)
     limit = sum(nvmlDeviceGetMemoryInfo(dev).free for dev in devices)
     nvmlShutdown()
-    return (limit // 2 ** 20), len(ids)
+    return (limit // 2**20), len(ids)
 
 
-def addr(x):
+def id_(x):
     return f'0x{id(x):x}'
+
+
+def as_tuple(x):
+    if isinstance(x, tuple):
+        return x
+    return (x, )
+
+
+def named(typed):
+    return type(typed).__name__.replace('Backward', '')
+
+
+def sized(tensor):
+    return f'{decimate(tensor.nelement() * tensor.element_size())}B'
+
+
+def hook(shapes: dict, m, *ts):
+    for t in ts:
+        shapes.update({v.grad_fn: tuple(v.shape) for v in as_tuple(t)})
 
 
 def plot_model(model: torch.nn.Module, *input_shapes: tuple, device='cpu'):
@@ -37,7 +60,10 @@ def plot_model(model: torch.nn.Module, *input_shapes: tuple, device='cpu'):
     Blue nodes are the Variables that require grad, orange are Tensors
     saved for backward in torch.autograd.Function
     """
-    dot = Digraph(
+    dot = graphviz.Digraph(
+        filename=getattr(model, 'name', type(model).__name__),
+        directory='graphs',
+        format='svg',
         node_attr={
             'style': 'filled',
             'shape': 'box',
@@ -46,59 +72,55 @@ def plot_model(model: torch.nn.Module, *input_shapes: tuple, device='cpu'):
             'ranksep': '0.1',
             'height': '0.2',
         },
-        graph_attr={'size': '12,12'},
-        format='svg',
     )
 
-    seen = set()
-    # batch_size = 2, otherwise BatchNorm will fail
-    inputs = (torch.randn(2, *shape, device=device) for shape in input_shapes)
-    var = model(*inputs)
-    if not isinstance(var, tuple):
-        var = (var, )
+    inputs = [torch.zeros(2, *shape, device=device, requires_grad=True)
+              for shape in input_shapes]
+    shapes = {}
+    with ExitStack() as stack:
+        model.apply(lambda m: stack.callback(
+            m.register_forward_hook(functools.partial(hook, shapes)).remove
+            ))
+        var = model(*inputs)
 
-    params = {addr(v): k for k, v in model.state_dict(keep_vars=True).items()}
+    var = as_tuple(var)
+    params = {id(v): k for k, v in model.state_dict(keep_vars=True).items()}
     outputs = tuple(v.grad_fn for v in var)
 
+    @functools.lru_cache(maxsize=None)  # call once for each unique `var`
     def add_nodes(var):
-        if var in seen:
-            return
-        seen.add(var)
-
         if torch.is_tensor(var):
-            dot.node(addr(var), tuple(var.shape), fillcolor='orange')
+            dot.node(id_(var), tuple(var.shape), fillcolor='orange')
         elif hasattr(var, 'variable'):
             u = var.variable
-            dot.node(addr(var), f'{params.get(addr(u))}\n {tuple(u.shape)}',
-                     fillcolor='lightblue')
-        elif var in outputs:
-            dot.node(addr(var), type(var).__name__.replace('Backward', ''),
-                     fillcolor='darkolivegreen1')
+            dot.node(
+                id_(var),
+                f'{params.get(id(u))} : {tuple(u.shape)} : {sized(u)}',
+                fillcolor=('lightblue', 'orange')[id(u) in map(id, inputs)]
+            )
         else:
-            dot.node(addr(var), type(var).__name__.replace('Backward', ''))
+            dot.node(
+                id_(var), named(var),
+                fillcolor=('lightgrey', 'darkolivegreen1')[var in outputs]
+            )
 
-        if hasattr(var, 'next_functions'):
-            for u, *_ in var.next_functions:
-                if u is not None:
-                    dot.edge(addr(var), addr(u))
-                    add_nodes(u)
-        if hasattr(var, 'saved_tensors'):
-            for t in var.saved_tensors:
-                dot.edge(addr(var), addr(t))
-                add_nodes(t)
+        for gen in (
+            (c for c, *_ in getattr(var, 'next_functions', ())),
+            getattr(var, 'saved_tensors', ()),
+        ):
+            for child in gen:
+                if child is None:
+                    continue
+                dot.edge(id_(child), id_(var),
+                         label=f'{shapes[child]}' if child in shapes else None)
+                add_nodes(child)
 
     for v in outputs:
         add_nodes(v)
 
-    size_max = 12
+    size_min = 12
     scale_factor = .15
-    size = max(size_max, len(dot.body) * scale_factor)
+    size = max(size_min, len(dot.body) * scale_factor)
     dot.graph_attr.update(size=f'{size},{size}')
-
-    os.makedirs('graphs', exist_ok=True)
-    try:
-        model_name = model.name
-    except AttributeError:
-        model_name = type(model).__name__
-    with open(f'graphs/{model_name}.svg', 'wb') as f:
-        f.write(dot.pipe())
+    dot.render(cleanup=True)
+    return dot
