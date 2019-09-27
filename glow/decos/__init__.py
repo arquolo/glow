7605b824a45f704bbe003f4d49d483faa5e81176
Paul M <arquolo@gmail.com>
2019-09-27 20:11:59 +0500

    Hotfix

--- glow/decos/__init__.py
+++ glow/decos/__init__.py
@@ -1,12 +1,12 @@
 # flake8: noqa
 from .cache import *
 from .thread import *
+from .reusable import *
 from .util import *
-from .timelimit import *
 
 __all__ = (
     cache.__all__ +
     thread.__all__ +
-    util.__all__ +
-    timelimit.__all__
+    reusable.__all__ +
+    util.__all__
 )
