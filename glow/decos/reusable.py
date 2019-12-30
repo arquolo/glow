188ca13269cb91a3fd5649c32ea87b83bac59f17
Paul M <arquolo@gmail.com>
2019-12-30 14:11:26 +0500

    Add type hints to decos.reusable

--- glow/decos/reusable.py
+++ glow/decos/reusable.py
@@ -1,62 +1,65 @@
 __all__ = ('Reusable', )
 
 import asyncio
+import threading
 import weakref
-from asyncio import AbstractEventLoop, Lock
-from threading import Thread
+from typing import Any, Callable, Generic, List, Optional, TypeVar
 
+from .thread import call_once
 
-from . import call_once
+_T = TypeVar('_T')
 
 
 @call_once
-def make_loop():
+def make_loop() -> asyncio.AbstractEventLoop:
     def start_loop(loop):
         asyncio.set_event_loop(loop)
         loop.run_forever()
 
     loop = asyncio.new_event_loop()
-    lock = Lock(loop=loop)
-    Thread(target=start_loop, args=(loop, ), daemon=True).start()
-    return loop, lock
+    threading.Thread(target=start_loop, args=(loop, ), daemon=True).start()
+    return loop
 
 
-class Reusable:
-    _loop: AbstractEventLoop = None
-    _lock: Lock = None
+def _finalize(_: Any) -> None:
+    pass
 
-    def __init__(self, factory, finalize=None, timeout: float = 0):
+
+class Reusable(Generic[_T]):
+    def __init__(self,
+                 factory: Callable[[], _T],
+                 finalize: Callable[[_T], None] = _finalize,
+                 timeout=0.0):
         self.factory = factory
-        self.finalize = finalize or (lambda _: None)
+        self.finalize = finalize
         self.timeout = timeout
 
-        self._loop, self._lock = make_loop()
-        self._deleter = None
+        self._loop = make_loop()
+        self._lock = asyncio.Lock(loop=self._loop)
+        self._deleter: Optional[asyncio.TimerHandle] = None
+        self._bx: List[_T] = []
 
-    def get(self):
+    def get(self) -> _T:
         """Returns inner object, or recreates it"""
         fut = asyncio.run_coroutine_threadsafe(self._get(), loop=self._loop)
         return fut.result()
 
-    def _delete(self):
-        del self.ref
-
     def _finalize(self, ref):
-        obj = ref()
+        obj: Optional[_T] = ref()
         if obj is not None:
             self.finalize(obj)
 
-    async def _get(self):
+    async def _get(self) -> _T:
         async with self._lock:
             # reschedule finalizer
             if self._deleter is not None:
                 self._deleter.cancel()
-            self._deleter = self._loop.call_later(self.timeout, self._delete)
+            self._deleter = self._loop.call_later(self.timeout, self._bx.clear)
 
             # recreate object, if nessessary
-            if not hasattr(self, 'ref'):
-                self.ref = self.factory()
-                ref = weakref.ref(self.ref)
-                weakref.finalize(self.ref, self._finalize, ref)
+            if not self._bx:
+                obj = self.factory()
+                weakref.finalize(obj, self._finalize, weakref.ref(obj))
+                self._bx.append(obj)
 
-            return self.ref
+            return self._bx[0]
