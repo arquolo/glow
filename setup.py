c355be7c7c2201eecbc5cc933711f6f235e3a3f7
Paul M <arquolo@gmail.com>
2020-02-10 15:53:34 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     url='https://github.com/arquolo/glow',
     author='Paul Maevskikh',
     author_email='arquolo@gmail.com',
-    description='Optimized RouTines for Python',
+    description='Toolset for model training and creation of pipelines',
     long_description=Path('README.md').read_text(),
     long_description_content_type='text/markdown',
     classifiers=[
@@ -18,6 +18,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
@@ -27,24 +28,24 @@ setuptools.setup(
         "dataclasses ; python_version<'3.7'",
         "pickle5 ; python_version<'3.8'",
         'loky',
-        'numpy>=1.15',
+        'numpy>=1.17',
         'psutil',
         'typing-extensions',
         'wrapt',
     ],
     extras_require={
         'cv': [
-            'future',  # torch.utils.tensorboard dies if missing
             'graphviz',
+            'matplotlib',
             'numba',
             'opencv-python>=4',
             'py3nvml',
             'pyaudio',
             'soundfile',
-            'torch>=1.2',
+            'torch>=1.4',
         ],
     },
     dependency_links=[
         'https://download.pytorch.org/whl/torch_stable.html',
-    ]
+    ],
 )
