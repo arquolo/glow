326856968c3d68fbddfb542d8f08ddaa17d06c6f
Paul M <arquolo@gmail.com>
2020-02-13 17:19:58 +0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name='glow',
-    version='0.8.1',
+    version='0.8.2',
     url='https://github.com/arquolo/glow',
     author='Paul Maevskikh',
     author_email='arquolo@gmail.com',
