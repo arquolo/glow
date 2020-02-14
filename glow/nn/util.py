91d478525d9167b34a5efb8715bd52efb0a1cd03
Paul M <arquolo@gmail.com>
2020-02-14 20:47:03 +0500

    Add some helpers for nn

--- glow/nn/util.py
+++ glow/nn/util.py
@@ -1,9 +1,31 @@
-__all__ = ('param_count', )
+__all__ = ('device', 'frozen', 'param_count')
 
+import contextlib
+from typing import Iterator
+
+import torch
 from torch import nn
 
 from ..core import Size
 
 
-def param_count(module: nn.Module) -> Size:
-    return Size(sum(p.numel() for p in module.parameters()), base=1000)
+def device() -> torch.device:
+    """Gets current device, including CPU"""
+    if torch.cuda.is_available():
+        return torch.device(torch.cuda.current_device())
+    return torch.device('cpu')
+
+
+@contextlib.contextmanager
+def frozen(net: nn.Module) -> Iterator[None]:
+    """Context manager which frozes `net` turning it to eval mode"""
+    with contextlib.ExitStack() as stack:
+        stack.callback(net.train, net.training)
+        stack.enter_context(torch.no_grad())
+        net.eval()
+        yield
+
+
+def param_count(net: nn.Module) -> Size:
+    """Count of parameters in `net`, both training and not"""
+    return Size(sum(p.numel() for p in net.parameters()), base=1000)
