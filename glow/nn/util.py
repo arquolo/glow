7bb7451fdcd5d6de4343ca9097a49a66085a0ce6
Paul M <arquolo@gmail.com>
2020-02-15 14:09:30 +0500

    Add CUDA profiler

--- glow/nn/util.py
+++ glow/nn/util.py
@@ -1,12 +1,15 @@
-__all__ = ('device', 'frozen', 'param_count')
+__all__ = ('device', 'frozen', 'param_count', 'profile')
 
 import contextlib
-from typing import Iterator
+import functools
+from typing import Callable, Iterator, TypeVar, cast
 
 import torch
 from torch import nn
 
 from ..core import Size
+
+_F = TypeVar('_F', bound=Callable[..., Iterator])
 
 
 def device() -> torch.device:
@@ -29,3 +32,25 @@ def frozen(net: nn.Module) -> Iterator[None]:
 def param_count(net: nn.Module) -> Size:
     """Count of parameters in `net`, both training and not"""
     return Size(sum(p.numel() for p in net.parameters()), base=1000)
+
+
+def profile(fn: _F) -> _F:
+    """Decorator to profile CUDA ops. Use with `nvprof`
+
+    Use in script launched via
+    `nvprof --profile-from-start off -o trace.prof -- python main.py`
+
+    >>> @profile
+    ... def train_loop():
+    ...     for data in loader:
+    ...         yield step(data)
+
+    """
+    def wrapper(*args, **kwargs):
+        results = fn(*args, **kwargs)
+        with torch.cuda.profiler.profile():
+            yield next(results)
+            with torch.autograd.profiler.emit_nvtx():
+                yield from results
+
+    return cast(_F, functools.update_wrapper(wrapper, fn))
