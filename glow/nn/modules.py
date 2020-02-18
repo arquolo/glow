b5a0ab9a54002232fbf07eccccb9a9ecbcbe68ec
Paul M <arquolo@gmail.com>
2020-02-18 21:53:46 +0500

    Update mish/swish activations to memory efficient

--- glow/nn/modules.py
+++ glow/nn/modules.py
@@ -53,11 +53,45 @@ class Noise(nn.Module):
         return f'std={self.std}'
 
 
-class Swish(nn.Module):
+class _ModuleBase(nn.Module):
+    class _AutoFn(torch.autograd.Function):
+        @classmethod
+        def forward(cls, ctx, x):
+            ctx.save_for_backward(x)
+            return cls._forward(x)
+
+        @classmethod
+        def backward(cls, ctx, grad):
+            return cls._backward(ctx.saved_tensors[0], grad)
+
     def forward(self, x):
-        return x.mul(x.sigmoid())
+        return self._AutoFn.apply(x)
 
 
-class Mish(nn.Module):
-    def forward(self, x):
-        return x.mul(functional.softplus(x).tanh())
+class Swish(_ModuleBase):
+    class _AutoFn(_ModuleBase._AutoFn):
+        @staticmethod
+        @torch.jit.script
+        def _forward(x):
+            return x.sigmoid().mul(x)
+
+        @staticmethod
+        @torch.jit.script
+        def _backward(x, grad):
+            sig = x.sigmoid()
+            return (sig * (1 + x * (1 - sig))).mul(grad)
+
+
+class Mish(_ModuleBase):
+    class _AutoFn(_ModuleBase._AutoFn):
+        @staticmethod
+        @torch.jit.script
+        def _forward(x):
+            return functional.softplus(x).tanh().mul(x)
+
+        @staticmethod
+        @torch.jit.script
+        def _backward(x, grad):
+            sig = x.sigmoid()
+            tanh = functional.softplus(x).tanh()
+            return (tanh + x * sig * (1 - tanh * tanh)).mul(grad)
