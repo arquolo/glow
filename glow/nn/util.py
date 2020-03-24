93340f2d9e31a8c19c573cec4c3c7b8e83c70c0c
Paul M <arquolo@gmail.com>
2020-03-24 20:39:29 +0500

    Add export to onnx

--- glow/nn/util.py
+++ glow/nn/util.py
@@ -1,10 +1,13 @@
-__all__ = ('device', 'frozen', 'inference', 'param_count', 'profile')
+__all__ = ('device', 'dump_to_onnx', 'frozen', 'inference', 'param_count',
+           'profile')
 
 import functools
-from typing import Callable, Iterator, TypeVar, cast
 from contextlib import ExitStack, contextmanager
+from io import BytesIO
+from typing import Callable, Iterator, Tuple, TypeVar, cast
 
 import torch
+import torch.onnx
 from torch import nn
 
 from ..core import Size
@@ -71,3 +74,42 @@ def profile(fn: _F) -> _F:
                 yield from results
 
     return cast(_F, functools.update_wrapper(wrapper, fn))
+
+
+def dump_to_onnx(net: nn.Module, *shapes: Tuple[int], device='cpu') -> bytes:
+    """Converts model to ONNX graph, represented as bytes
+
+    Parameters:
+      - `net`: torch.nn.Module to convert
+      - `shapes`: Shapes of input data, all except batch dimension
+
+    Example usage:
+
+    >>> net: torch.nn.Linear(4, 4)
+    >>> bytes_ = dump_to_onnx(net, [4])
+
+    To restore graph:
+
+    >>> from onnxruntime import backend
+    >>> rep = backend.prepare(bytes_or_filename, device='cpu')
+    >>> rep.run([np.zeros(4, 4)])[0]
+
+    """
+    dynamic_axes = {
+        f'inp_{i}': {
+            0: 'batch',
+            **{dim: f'inp_{i}_dim_{dim}' for dim in range(2, 1 + len(shape))}
+        } for i, shape in enumerate(shapes)
+    }
+    buf = BytesIO()
+    torch.onnx.export(
+        net.to(device).eval(),
+        tuple(
+            torch.randn(1, *shape, requires_grad=True, device=device)
+            for shape in shapes),
+        buf,
+        input_names=[*dynamic_axes],
+        dynamic_axes=dynamic_axes,
+        opset_version=11,
+        do_constant_folding=True)
+    return buf.getvalue()
