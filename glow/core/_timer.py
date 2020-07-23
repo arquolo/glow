bf7db9084c6ea595ca687a7b9c6348f670ba8801
Paul M <arquolo@gmail.com>
2020-07-23 23:30:06 +0500

    Update core.timer

--- glow/core/_timer.py
+++ glow/core/_timer.py
@@ -1,69 +1,57 @@
-__all__ = ('timer', 'time_this')
+__all__ = ['time_this', 'timer']
 
 import contextlib
 import functools
 import threading
 import time
 import weakref
-from dataclasses import dataclass
-from typing import Callable, DefaultDict, TypeVar, cast
+from typing import Callable, Dict, TypeVar, cast
+
+from ._repr import Si
 
 _F = TypeVar('_F', bound=Callable)
 
 
 @contextlib.contextmanager
 def timer(name: str = 'Task', callback: Callable[[float], object] = None):
-    if callback is None:
-
-        def callback(duration: float) -> None:
-            print(f'{name} done in {duration:.4g} seconds')
-
-    assert callback is not None
-    with contextlib.ExitStack() as stack:
-        stack.callback(
-            lambda init: callback(time.perf_counter() - init),  # type: ignore
-            time.perf_counter(),
-        )
+    init = time.perf_counter()
+    try:
         yield
-
-
-@dataclass
-class _ThreadInfo:
-    count: int = 0
-    duration: float = 0
-
-    def update(self, start: float) -> None:
-        duration = time.perf_counter() - start
-        self.count += 1
-        self.duration += (duration - self.duration) / self.count
+    finally:
+        duration = time.perf_counter() - init
+        if callback is not None:
+            callback(duration)
+        else:
+            print(f'{name} done in {duration:.4g} seconds')
 
 
 def time_this(fn: _F) -> _F:
-    lock = threading.RLock()
-    infos = DefaultDict[str, _ThreadInfo](_ThreadInfo)
+    """Log function timings at program exit"""
+    infos: Dict[int, list] = {}
 
     def finalize(start):
         if not infos:
             return
 
-        call_count = sum(i.count for i in infos.values())
-        total_time = sum(i.count * i.duration for i in infos.values())
+        num_calls = sum(num for num, _ in infos.values())
+        total_time = sum(num * duration for num, duration in infos.values())
         total_runtime = time.perf_counter() - start + 1e-7
 
         print(f'{fn.__module__}:{fn.__qualname__} -'
-              f' calls: {call_count},'
-              f' total: {total_time:.4f},'
-              f' per-call: {total_time / call_count:.4f}'
+              f' calls: {num_calls},'
+              f' total: {Si(total_time)}s,'
+              f' per-call: {Si(total_time / num_calls)}s'
               f' ({100 * total_time / total_runtime:.2f}% of module),'
               f' threads: {len(infos)}')
 
-    def wrapper(*args, **kwargs):
-        with lock:
-            info = infos[threading.get_ident()]
+    def callback(duration: float):
+        info = infos.setdefault(threading.get_ident(), [0, 0.])
+        info[0] += 1
+        info[1] += (duration - info[1]) / info[0]
 
-        with contextlib.ExitStack() as stack:
-            stack.callback(info.update, time.perf_counter())
-            return fn(*args, **kwargs)
+    @timer(callback=callback)
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
 
     finalizer = weakref.finalize(fn, finalize, time.perf_counter())
     wrapper.finalize = finalizer  # type: ignore
