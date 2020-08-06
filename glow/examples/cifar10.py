888b8d280caca2c074a5b93b20d4b32d4469fe9c
Paul M <arquolo@gmail.com>
2020-08-06 21:13:06 +0500

    Update examples

--- glow/examples/cifar10.py
+++ glow/examples/cifar10.py
@@ -1,18 +1,26 @@
 import argparse
 import pathlib
-from dataclasses import dataclass
-from typing import Callable, DefaultDict, Iterable, List
+from typing import DefaultDict
 
 import glow
-import glow.nn as gnn
 import glow.metrics as m
+import glow.nn as gnn
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
+import torch.cuda.amp
 import torch.nn as nn
-import torch.optim
-from matplotlib import pyplot as plt
-from torchvision import datasets
+from torchvision.datasets import CIFAR10
 from torchvision import transforms as tfs
 from tqdm.auto import tqdm
+
+DEVICE = torch.device('cuda')
+
+glow.lock_seed(42)
+torch.backends.cudnn.benchmark = True  # type: ignore
+rg = np.random.default_rng()
+
+# ------------------------------ define model ------------------------------
 
 
 def make_model_default():
@@ -52,130 +60,95 @@ def make_model_new(init=16):
                 conv(cout * 2, pad=2, groups=cout * 2),
                 conv(cout * 2, cout)[:-1],
                 tail=nn.ReLU(),
-                skip=0.1))
+                skip=0.1),
+        )
 
     return nn.Sequential(
-        # > 32^2
-        # nn.ZeroPad2d(2),
-        # > 16^2
-        # conv_down(1, init),
-        # > 16^2
-        conv_down(3, init),
-        # > 8^2
-        conv_down(init, init * 2),
-        # > 4^2
-        conv_down(init * 2, init * 4),
+        conv_down(3, init),  # > 16^2
+        conv_down(init, init * 2),  # > 8^2
+        conv_down(init * 2, init * 4),  # > 4^2
         conv(init * 4, init * 8, pad=2),
-        # > 1
-        nn.AdaptiveAvgPool2d(1),
+        nn.AdaptiveAvgPool2d(1),  # > 1
         gnn.View(-1),
         nn.Linear(init * 8, 10),
     )
 
 
-@dataclass
-class Engine:
-    net: nn.Module
-    optim: torch.optim.Optimizer
-    criterion: Callable
-    metrics: Iterable[m.Metric]
+# parse args
 
-    def _step(self, data, target, is_train):
-        self.net.train(is_train)
-        with torch.set_grad_enabled(is_train):
-            out = self.net(data)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'root', type=pathlib.Path, help='location of cifar10/ folder')
+parser.add_argument('--batch-size', type=int, default=4, help='train batch')
+parser.add_argument('--epochs', type=int, default=12, help='count of epochs')
+parser.add_argument('--steps-per-epoch', type=int, help='steps per epoch')
+parser.add_argument('--width', type=int, default=32, help='width of network')
+parser.add_argument(
+    '--fp16', action='store_true', help='enable mixed precision mode')
+parser.add_argument('--plot', action='store_true', help='disable plot')
 
-        if is_train:
-            self.optim.zero_grad()
-            self.criterion(out, target).backward()
-            self.optim.step()
+args = parser.parse_args()
 
-        return out.detach()
+epoch_len = (8000 // args.batch_size
+             if args.steps_per_epoch is None else args.steps_per_epoch)
+sample_size = args.epochs * epoch_len * args.batch_size
 
-    def run(self, loader: Iterable, is_train: bool = True):
-        meter = m.compose(*self.metrics)
-        for data, target in loader:
-            data, target = data.cuda(), target.cuda()
-            out = self._step(data, target, is_train)
-            scores = meter.send((out, target))
-            yield {k: v.item() for k, v in scores.items() if v.numel() == 1}
+tft = tfs.Compose([
+    tfs.RandomCrop(32, padding=4),
+    tfs.RandomHorizontalFlip(),
+    tfs.ToTensor(),
+])
+ds = CIFAR10(args.root / 'cifar10', transform=tft, download=True)
+ds_val = CIFAR10(args.root / 'cifar10', transform=tfs.ToTensor(), train=False)
 
 
-def main(root: pathlib.Path, batch_size: int, width: int, epochs: int):
-    tft = tfs.Compose([
-        tfs.RandomCrop(32, padding=4),
-        tfs.RandomHorizontalFlip(),
-        tfs.ToTensor(),
-        tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-    tfv = tfs.Compose([
-        tfs.ToTensor(),
-        tfs.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-    ])
-    path = root / 'cifar10'
-    tset = datasets.CIFAR10(path, transform=tft, train=True, download=True)
-    vset = datasets.CIFAR10(path, transform=tfv, train=False)
-    """
-    tf = tfs.Compose([
-        tfs.ToTensor(),
-        tfs.Normalize((0.5,), (0.5,)),
-    ])
-    tset = datasets.MNIST(root, transform=tf, train=True, download=True)
-    vset = datasets.MNIST(root, transform=tf, train=False)
-    """
-    @glow.repeatable(hint=tset.__len__)
-    def sampler():
-        return torch.randperm(len(tset))
+@glow.repeatable(hint=lambda: sample_size)
+def sampler():
+    return rg.integers(len(ds), size=sample_size)
 
-    tload = gnn.make_loader(
-        tset, sampler(), batch_size=batch_size, multiprocessing=False)
-    vload = gnn.make_loader(vset, batch_size=200, multiprocessing=False)
 
-    # net = make_model_default()
-    net = make_model_new(init=width)
-    net.cuda()
-    print('params:', gnn.param_count(net))
+loader = gnn.make_loader(
+    ds, sampler(), batch_size=args.batch_size, multiprocessing=False)
+val_loader = gnn.make_loader(ds_val, batch_size=100, multiprocessing=False)
 
-    optim = gnn.RAdam(net.parameters())
-    loss_fn = nn.CrossEntropyLoss()
+# net = make_model_default()
+net = make_model_new(args.width)
+net.to(DEVICE)
+opt = torch.optim.AdamW(net.parameters())
+print(gnn.param_count(net))
 
-    metrics = [
-        m.Lambda(loss_fn, name='loss'),
-        m.Confusion(acc=m.accuracy, kappa=m.kappa),
-    ]
+criterion = nn.CrossEntropyLoss()
+metrics = [
+    m.Lambda(criterion, name='loss'),
+    m.Confusion(acc=m.accuracy, kappa=m.kappa),
+]
+stepper = gnn.Stepper(
+    net, opt, criterion, metrics, device=DEVICE, fp16=args.fp16)
 
-    scores = DefaultDict[str, List](list)
-    engine = Engine(net, optim, loss_fn, metrics)
+history = DefaultDict[str, list](list)
+with tqdm(total=epoch_len * args.epochs, desc='train') as pbar:
+    for i, split in enumerate(glow.ichunked(loader, epoch_len), 1):
+        tscalars = stepper.run(split, pbar).scalars
+        with tqdm(total=len(val_loader), desc='val', leave=False) as pbar_val:
+            vscalars = stepper.run(val_loader, pbar_val, False).scalars
 
-    for _ in tqdm(range(epochs), desc='epochs'):
-        for split in glow.ichunked(
-                tqdm(tload, desc='train', leave=False),
-                size=8000 // batch_size):
-            *_, mt = engine.run(split)
-            *_, mv = engine.run(
-                tqdm(vload, desc='val', leave=False), is_train=False)
+        _tags = sorted({*tscalars, *vscalars})
+        scores = {tag: [tscalars[tag], vscalars[tag]] for tag in _tags}
 
-            names = sorted({*mt} & {*mv})
-            # print(', '.join(f'{k}: {mt[k]:.3f}/{mv[k]:.3f}' for k in names))
-            for name in names:
-                scores[name].append([mt[name], mv[name]])
+        for tag, values in scores.items():
+            history[tag].append(values)
+        scalars_fmt = ', '.join(f'{tag}: {{:.3f}}/{{:.3f}}'.format(*values)
+                                for tag, values in scores.items())
+        print(f'[{i:03d}] {scalars_fmt}')
 
-    _, axes = plt.subplots(ncols=len(scores))
-    for (title, data), ax in zip(scores.items(), axes):
-        ax.legend(ax.plot(data), ['train', 'val'])
-        ax.set_title(title)
+if args.plot:
+    fig = plt.figure(f'batch_size={args.batch_size}, fp16={args.fp16}')
+    for i, (tag, values) in enumerate(history.items(), 1):
+        ax = fig.add_subplot(1, len(history), i)
+        ax.legend(ax.plot(values), ['train', 'val'])
+        ax.set_title(tag)
+        ax.set_ylim([
+            int(min(x for xs in values for x in xs)),
+            int(max(x for xs in values for x in xs) + 0.999)
+        ])
     plt.show()
-    return [max(ks) for ks in zip(*scores['kappa'])]
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'root', type=pathlib.Path, help='location to store dataset')
-    parser.add_argument(
-        '--batch-size', type=int, default=4, help='batch size for train')
-    parser.add_argument('--epochs', type=int, default=2, help='epochs')
-    parser.add_argument(
-        '--width', type=int, default=32, help='width of network')
-    args = parser.parse_args()
-    main(**vars(args))
