4cb5580de44024fa656514d5323f3a6ec5980037
Paul M <arquolo@gmail.com>
2020-11-25 17:59:34 +0500

    Refactor buffered as class. Add multiprocessing

--- glow/core/pipe/buffer.py
+++ glow/core/pipe/buffer.py
@@ -1,13 +1,17 @@
 __all__ = ['buffered']
 
-import contextlib
+import atexit
 import enum
-import queue
 import threading
-from concurrent import futures
-from typing import TypeVar, Union
+from concurrent.futures import Executor, ThreadPoolExecutor
+from contextlib import ExitStack
+from dataclasses import dataclass
+from queue import Queue
+from typing import (Callable, Iterable, Iterator, Optional, Protocol, Tuple,
+                    TypeVar, Union)
 
-from .len_helpers import MaybeSizedIterable, as_sized
+import loky
+import loky.backend
 
 
 class _Empty(enum.Enum):
@@ -19,31 +23,83 @@ _MaybeEmpty = Union[_T, _Empty]
 _empty = _Empty.token
 
 
-@as_sized(hint=lambda it, _: len(it))
-def buffered(iterable: MaybeSizedIterable[_T],
-             latency: int = 2) -> MaybeSizedIterable[_T]:
+class _IQueue(Protocol[_T]):
+    def get(self) -> _T:
+        ...
+
+    def put(self, value: _T) -> None:
+        ...
+
+
+class _IEvent(Protocol):
+    def is_set(self) -> bool:
+        ...
+
+    def set(self) -> None:  # noqa: A003
+        ...
+
+
+def _setup(
+    stack: ExitStack,
+    latency: int,
+    num_workers: int,
+) -> Tuple[Executor, _IQueue, _IEvent, Optional[Callable]]:
+    ex: Executor
+    if not num_workers:
+        return stack.enter_context(
+            ThreadPoolExecutor(1)), Queue(latency), threading.Event(), None
+
+    context = loky.backend.context.get_context('loky_init_main')
+    ex = loky.get_reusable_executor(num_workers, context)  # type: ignore
+    mgr = stack.enter_context(context.Manager())
+    return (ex, mgr.Queue(latency), mgr.Event(),
+            atexit.register(ex.shutdown, kill_workers=True))
+
+
+@dataclass
+class _Buffered(Iterable[_T]):
+    iterable: Iterable[_T]
+    latency: int = 2
+    num_workers: int = 0
+
+    def _consume(self, q: _IQueue[_MaybeEmpty[_T]], stop: _IEvent):
+        with ExitStack() as stack:
+            stack.callback(q.put, _empty)  # Match last q.get
+            stack.callback(q.put, _empty)  # Signal to stop iteration
+
+            for item, _ in zip(self.iterable, iter(stop.is_set, True)):
+                q.put(item)
+
+            # if stop.is_set():
+            #     stack.pop_all()
+            # q.put(_empty)  # Will happen if all ok to notify main to stop
+
+    def __iter__(self) -> Iterator[_T]:
+        with ExitStack() as stack:
+            q: _IQueue[_MaybeEmpty[_T]]
+            ex, q, stop, terminator = _setup(stack, self.latency,
+                                             self.num_workers)
+
+            stack.callback(q.get)  # Wakes q.put when main fails
+            stack.callback(stop.set)
+
+            task = ex.submit(self._consume, q, stop)
+            while (item := q.get()) is not _empty:
+                yield item
+            task.result()  # Throws if `consume` is dead
+
+        if terminator is not None:  # Cancel killing workers if all ok
+            atexit.unregister(terminator)
+
+    def __len__(self) -> int:
+        return len(self.iterable)  # type: ignore
+
+
+def buffered(iterable: Iterable[_T],
+             latency: int = 2,
+             num_workers: int = 0) -> _Buffered[_T]:
     """
     Iterates over `iterable` in background thread with at most `latency`
     items ahead from caller
     """
-    q: 'queue.Queue[_MaybeEmpty[_T]]' = queue.Queue(latency)
-    stop = threading.Event()
-
-    def consume():
-        with contextlib.ExitStack() as push:
-            push.callback(q.put, _empty)  # match last q.get
-            push.callback(q.put, _empty)  # signal to stop iteration
-            for item, _ in zip(iterable, iter(stop.is_set, True)):
-                q.put(item)
-            if stop.is_set():
-                push.pop_all()
-
-    with contextlib.ExitStack() as pull:
-        src = pull.enter_context(futures.ThreadPoolExecutor(1))
-        pull.callback(q.get)  # unlock q.put in consume on Exception in main
-        pull.callback(stop.set)
-
-        task = src.submit(consume)
-        while (item := q.get()) is not _empty:
-            yield item
-        task.result()  # throws if `consume` is dead
+    return _Buffered(iterable, latency, num_workers)
