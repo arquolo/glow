3457d9d9db4a5de45c7aaf05cae57b64412ca497
Paul M <arquolo@gmail.com>
2020-12-22 22:24:17 +0500

    Add basic support for DDP

---
+++ glow/distributed.py
@@ -0,0 +1,152 @@
+__all__ = [
+    'auto_ddp', 'auto_model', 'barrier', 'get_rank', 'get_world_size',
+    'reduce_if_needed'
+]
+
+import pickle
+from pathlib import Path
+from functools import partial, update_wrapper
+from typing import Any, Callable, Optional, Protocol, Tuple, TypeVar, cast
+
+import torch
+import torch.cuda
+import torch.distributed as dist
+import torch.multiprocessing as mp
+from torch import nn
+
+# -------------------------------- primitives --------------------------------
+
+
+def get_rank() -> int:
+    """
+    In distributed context returns the rank of current process group. otherwise
+    returns -1.
+    """
+    return dist.get_rank() if dist.is_initialized() else -1
+
+
+def get_world_size() -> int:
+    """
+    In distributed context returns number of processes in the current process
+    group, otherwise returns 0.
+    """
+    return dist.get_world_size() if dist.is_initialized() else 0
+
+
+def barrier(rank: Optional[int] = None) -> None:
+    """Synchronize all processes"""
+    if get_world_size() > 1 and (rank is None or rank == get_rank()):
+        dist.barrier()
+
+
+def reduce_if_needed(*tensors: torch.Tensor,
+                     mean: bool = False) -> Tuple[torch.Tensor, ...]:
+    """Reduce tensors across all machines"""
+    if (world := get_world_size()) > 1:
+        tensors = *(t.clone() for t in tensors),
+        for op in [dist.all_reduce(t, async_op=True) for t in tensors]:
+            op.wait()
+        if mean:
+            tensors = *(t / world for t in tensors),
+    return tensors
+
+
+# --------------------------------- wrappers ---------------------------------
+
+
+def auto_model(net: nn.Module, sync_bn: bool = True) -> nn.Module:
+    if (rank := get_rank()) >= 0:
+        torch.cuda.set_device(rank)
+
+        net.to(rank)
+        if sync_bn:
+            net = nn.SyncBatchNorm.convert_sync_batchnorm(net)
+        return nn.parallel.DistributedDataParallel(net, device_ids=[rank])
+
+    net.cuda()
+    return (nn.parallel.DataParallel(net)
+            if torch.cuda.device_count() > 1 else net)
+
+
+class _TrainFn(Protocol):
+    def __call__(self, __net: nn.Module, *args, **kwargs) -> Any:
+        ...
+
+
+_F = TypeVar('_F', bound=Callable)
+_TrainFnType = TypeVar('_TrainFnType', bound=_TrainFn)
+
+
+class _AutoDdp:
+    def __init__(self, train_fn: _TrainFn, net: nn.Module, *args, **kwargs):
+        self.train_fn = train_fn
+        self.net = net
+        self.args = args
+        self.kwargs = kwargs
+        self.ngpus = torch.cuda.device_count()
+
+        if self.ngpus == 1:
+            self._worker(None)
+            return
+
+        # ! Not tested
+        # * Actually, here we can use loky.ProcessPoolExecutor, like this:
+        # from . import mapped
+        # jobs = mapped(
+        #     self._worker, range(self.ngpus), num_workers=self.ngpus, mp=True)
+        # list(jobs)
+        # * Left as safe measure
+        mp.spawn(self._worker, nprocs=self.ngpus)
+
+    def _worker(self, rank: Optional[int]) -> None:
+        if rank is None:
+            return self.train_fn(self.net, *self.args, **self.kwargs)
+
+        dist.init_process_group(
+            backend='nccl', rank=rank, world_size=self.ngpus)
+        try:
+            self.train_fn(auto_model(self.net), *self.args, **self.kwargs)
+        finally:
+            dist.destroy_process_group()
+
+
+def auto_ddp(train_fn: _TrainFnType) -> _TrainFnType:
+    return cast(_TrainFnType,
+                update_wrapper(partial(_AutoDdp, train_fn), train_fn))
+
+
+def once_per_world(fn: _F) -> _F:
+    """Call function only in rank=0 process, and share result for others"""
+
+    def wrapper(*args, **kwargs):
+        rank = get_rank()
+        world = get_world_size()
+
+        # Generate random fname and share it among whole world
+        idx = torch.empty((), dtype=torch.int64).random_()
+        if rank == 0:
+            dist.broadcast(idx, 0)
+        tmp = Path(f'/tmp/_ddp_share_{idx.item():x}.pkl')
+        result = None
+
+        if rank <= 0:  # master
+            result = fn(*args, **kwargs)
+            if world <= 1:
+                return result  # only master exists
+            with tmp.open('wb') as fp:
+                pickle.dump(result, fp)
+
+        barrier()
+
+        if rank > 0:  # slave
+            with tmp.open('rb') as fp:
+                result = pickle.load(fp)
+
+        barrier()
+
+        if rank == 0 and world > 1:  # parent
+            tmp.unlink()
+
+        return result
+
+    return cast(_F, update_wrapper(wrapper, fn))
--- glow/nn/_loader.py
+++ glow/nn/_loader.py
@@ -1,114 +1,239 @@
 __all__ = ['make_loader']
 
 import os
-from typing import Any, Callable, Iterator, NamedTuple, Optional, Tuple, Union
+import warnings
+from collections import abc
+from dataclasses import dataclass
+from typing import Any, Optional, Protocol, Sequence, Union
 
 import torch
-from torch.utils.data import (Dataset, IterableDataset, RandomSampler, Sampler,
-                              SequentialSampler)
+from torch.utils.data import Dataset, IterableDataset, Sampler
 from torch.utils.data._utils import worker as torch_worker
 
-from .. import buffered, chunked, mapped, partial_iter, roundrobin
+from .. import buffered, chunked, mapped, roundrobin
 from ..core._parallel import _get_executor
+from ..distributed import get_rank, get_world_size
 
-_CollateFn = Callable[[Any], Tuple[torch.Tensor, ...]]
 _NUM_CPUS: int = os.cpu_count() or 1
 
 
-def _default_collate(batch) -> Tuple[torch.Tensor, ...]:
+class _CollateFn(Protocol):
+    def __call__(self, __items: abc.Sequence) -> Any:
+        ...
+
+
+def default_collate(batch: Sequence[tuple]) -> Any:
     return tuple(
         torch.stack([torch.as_tensor(item) for item in row])
         for row in zip(*batch))
 
 
+def pin_memory(data):
+    if isinstance(data, torch.Tensor):
+        return data.pin_memory()
+    if hasattr(data, 'pin_memory'):
+        return data._pin_memory()
+
+    if isinstance(data, (str, bytes)):
+        return data
+    if isinstance(data, tuple) and hasattr(data, '_fields'):  # namedtuple
+        return type(data)(*(pin_memory(sample) for sample in data))
+    if isinstance(data, abc.Sequence):
+        return [pin_memory(sample) for sample in data]
+
+    if isinstance(data, abc.Mapping):
+        return {k: pin_memory(sample) for k, sample in data.items()}
+
+    return data
+
+
+# ------------------------------- base loader -------------------------------
+
+
+@dataclass(frozen=True)
+class _BaseLoader:
+    batch_size: int
+    num_workers: int
+    collate_fn: _CollateFn
+    pin_memory: bool
+    dataset: Dataset
+
+    def _iter_samples(self) -> abc.Iterator:
+        raise NotImplementedError
+
+    def __iter__(self) -> abc.Iterator:
+        batches = chunked(self._iter_samples(), self.batch_size)
+        batches = map(self.collate_fn, batches)
+        if not self.pin_memory:
+            return batches
+        return iter(mapped(pin_memory, batches, num_workers=1))
+
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+
 # ---------------------- loader for map-style datasets ----------------------
 
 
-class _MapLoader(NamedTuple):
-    dataset: Dataset
-    batch_size: int
+@dataclass(frozen=True)
+class _MapLoader(_BaseLoader):
     sampler: Sampler
-    num_workers: int
     mp: bool
-    collate_fn: Optional[_CollateFn]
     chunksize: Optional[int]
 
-    def __iter__(self) -> Iterator[Tuple[torch.Tensor, ...]]:
+    def _iter_samples(self) -> abc.Iterator:
+        num_workers = self.num_workers
+        if (world := get_world_size()) > 1:
+            num_workers //= world
+
+        if not num_workers:
+            return map(self.dataset.__getitem__, self.sampler)
+
         samples = mapped(
             self.dataset.__getitem__,
             self.sampler,
             mp=self.mp,
             chunksize=self.chunksize,
-            num_workers=self.num_workers)
-        batches = chunked(samples, self.batch_size)
-        return (batches if self.collate_fn is None  # type: ignore
-                else map(self.collate_fn, batches))
+            num_workers=num_workers)
+        return iter(samples)
 
     def __len__(self) -> int:
         indices = range(0, len(self.sampler), self.batch_size)  # type: ignore
         return len(indices)
 
+    def set_epoch(self, epoch: int):
+        if isinstance(self.sampler, _AutoSampler):
+            self.sampler.set_epoch(epoch)
+
 
 # -------------------- loader for iterable-style datasets --------------------
 
 
-class _WorkerInfo(NamedTuple):
+@dataclass(frozen=True)
+class _Worker:
     dataset: IterableDataset
     id: int  # noqa: A003, shadowing builtin `id`, false positive
     num_workers: int
     seed: Optional[int] = None
 
+    def __iter__(self) -> abc.Iterator:
+        torch_worker._worker_info = self
+        try:
+            yield from self.dataset
+        finally:
+            torch_worker._worker_info = None
 
-def _worker_fn(dataset: IterableDataset,
-               idx: int,
-               num_workers: int,
-               seed: int = None):
-    torch_worker._worker_info = _WorkerInfo(dataset, idx, num_workers, seed)
-    return iter(dataset)
 
-
-class _IterableLoader(NamedTuple):
+@dataclass(frozen=True)
+class _IterableLoader(_BaseLoader):
     dataset: IterableDataset
-    batch_size: int
-    num_workers: int
-    mp: bool
-    collate_fn: Optional[_CollateFn]
-    seed: Optional[int]
 
-    def __iter__(self) -> Iterator[Tuple[torch.Tensor, ...]]:
-        with _get_executor(self.num_workers, self.mp) as executor:
-            workers = [
-                buffered(
-                    partial_iter(_worker_fn)(self.dataset, idx,
-                                             self.num_workers, self.seed),
-                    mp=executor) for idx in range(self.num_workers)
-            ]
-            batches = chunked(roundrobin(*workers), self.batch_size)
-            if self.collate_fn is None:
-                yield from batches  # type: ignore
-            else:
-                yield from map(self.collate_fn, batches)
+    def _iter_samples(self) -> abc.Iterator:
+        if not self.num_workers:
+            yield from buffered(self.dataset)
+            return
+
+        seed = torch.empty((), dtype=torch.int64).random_().item()
+        workers = [
+            _Worker(self.dataset, idx, self.num_workers, int(seed))
+            for idx in range(self.num_workers)
+        ]
+        with _get_executor(self.num_workers, True) as executor:
+            yield from roundrobin(*(buffered(w, mp=executor) for w in workers))
 
     def __len__(self) -> int:
         indices = range(0, len(self.dataset), self.batch_size)  # type: ignore
         return len(indices)
 
 
+# --------------------------------- samplers ---------------------------------
+
+
+class _IAutoSampler(Sampler):
+    def __init__(self, source: Union[Dataset, Sampler]) -> None:
+        if not isinstance(source, abc.Sized):
+            raise TypeError('Argument should have length')
+
+        self.source = source
+        self.epoch = 0
+        self.seed = int(torch.empty((), dtype=torch.int64).random_().item())
+
+    def _indices(self) -> list:
+        raise NotImplementedError
+
+    def __iter__(self) -> abc.Iterator:
+        indices = self._indices()
+
+        if (world := get_world_size()) > 1:
+            if remainder := (len(indices) % world):
+                indices += indices[:world - remainder]
+            indices = indices[get_rank()::world]
+
+        if len(indices) != len(self):
+            raise RuntimeError(f'{len(indices)} vs {len(self)}')
+
+        return iter(indices)
+
+    def __len__(self) -> int:
+        world = get_world_size() or 1
+        return (len(self.source) + world - 1) // world
+
+    def set_epoch(self, epoch: int) -> None:
+        self.epoch = epoch
+
+
+class _AutoSampler(_IAutoSampler):
+    def __init__(self, dataset: Dataset, shuffle: bool = False):
+        if not isinstance(dataset, abc.Sized):
+            raise TypeError('Argument sampler should have length')
+
+        super().__init__(dataset)
+        self.shuffle = shuffle
+
+    def _indices(self) -> list:
+        if not self.shuffle:
+            return list(range(len(self.source)))
+
+        rng = torch.Generator()
+        rng.manual_seed(self.seed + self.epoch)
+        return torch.randperm(len(self.source), generator=rng).tolist()
+
+
+class _AutoSamplerProxy(_IAutoSampler):
+    def _indices(self) -> list:
+        torch.manual_seed(self.seed + self.epoch)
+        return [*self.source]  # type: ignore
+
+
 # ----------------------------- factory function -----------------------------
 
 
-def make_loader(
-    dataset: Dataset,
-    batch_size: int,
-    sampler: Sampler = None,
-    num_workers: int = _NUM_CPUS,
-    multiprocessing: bool = True,
-    shuffle: bool = False,
-    collate_fn: Optional[_CollateFn] = _default_collate,
-    chunk_from_batch: bool = False,
-    seed: int = None,
-) -> Union[_MapLoader, _IterableLoader]:
-    """Yields batches of batch_size from dataset in order from sampler.
+def make_loader(dataset: Dataset,
+                batch_size: int,
+                shuffle: bool = False,
+                sampler: Sampler = None,
+                num_workers: int = _NUM_CPUS,
+                collate_fn: _CollateFn = default_collate,
+                pin_memory: bool = False,
+                multiprocessing: bool = True,
+                chunk_from_batch: bool = False) -> _BaseLoader:
+    """
+    Data loader. Combines a dataset and a sampler, and provides an iterable
+    over the given dataset.
+
+    The data loader supports both map-style and iterable-style datasets with
+    single- or multi-process loading, customizing loading order and
+    automatic batching (collation) and memory pinning.
+
+    Yields batches of batch_size from dataset in order from sampler.
+
+    Differences from torch.utils.data.DataLoader:
+    - Support of threadpool backend for map-style datasets.
+    - Automatically adjusts chunksize of data for passing to the workers to
+      reduce IPC overhead when multiple processes are used.
+    - Automatically adjusts number of workers in distributed context for
+      map-style datasets.
+    - Use set_epoch() method before __iter__() for reprocucibility.
 
     Parameters:
     - batch_size - size of batch, each workers computes batch independently.
@@ -119,15 +244,36 @@ def make_loader(
       estimated automatically.
     """
     if isinstance(dataset, IterableDataset):
-        if sampler is not None:
-            raise ValueError('For IterableDataset sampler is not supported')
-        return _IterableLoader(dataset, batch_size, num_workers,
-                               multiprocessing, collate_fn, seed)
+        if shuffle or sampler is not None:
+            raise ValueError(
+                'Loader with IterableDataset: sampler/shuffle options are '
+                'not supported')
 
-    if sampler is None:
-        sampler_fn = RandomSampler if shuffle else SequentialSampler
-        sampler = sampler_fn(dataset)  # type: ignore
+        if not multiprocessing and num_workers != 0:
+            warnings.warn(
+                'Loader with IterableDataset: ThreadPool is not supported. '
+                'Setting num_workers to 0')
+            num_workers = 0
 
-    chunksize = batch_size if multiprocessing and chunk_from_batch else None
-    return _MapLoader(dataset, batch_size, sampler, num_workers,
-                      multiprocessing, collate_fn, chunksize)
+        if get_world_size() > 1:
+            raise ValueError(
+                'Loader with IterableDataset: distributed context is not '
+                'supported')
+
+        return _IterableLoader(batch_size, num_workers, collate_fn, pin_memory,
+                               dataset)
+
+    else:
+        if shuffle and sampler is not None:
+            raise ValueError(
+                'Loader with MapDataset: sampler option is mutually exclusive '
+                'with shuffle')
+
+        if sampler is None:
+            sampler = _AutoSampler(dataset, shuffle=shuffle)
+        else:
+            sampler = _AutoSamplerProxy(sampler)
+
+        chunksize = batch_size if multiprocessing and chunk_from_batch else None
+        return _MapLoader(batch_size, num_workers, collate_fn, pin_memory,
+                          dataset, sampler, multiprocessing, chunksize)
