a7821a91b6e3feabbe525aed99562785d48ec464
Paul M <arquolo@gmail.com>
2020-12-22 22:22:08 +0500

    Disable some exports

--- glow/core/__init__.py
+++ glow/core/__init__.py
@@ -1,5 +1,5 @@
 from . import _patch_len, _patch_print, _patch_scipy
-from ._len_helpers import as_sized, partial_iter
+from ._len_helpers import as_sized
 from ._more import (as_iter, chunked, eat, ichunked, roundrobin, sliced,
                     windowed)
 from ._parallel import buffered, mapped
@@ -13,10 +13,9 @@ from .wrap import (Reusable, call_once, interpreter_lock, memoize, shared_call,
 __all__ = [
     'as_iter', 'as_sized', 'buffered', 'call_once', 'chunked', 'coroutine',
     'countable', 'eat', 'ichunked', 'interpreter_lock', 'lock_seed', 'mangle',
-    'mapped', 'memoize', 'memprof', 'partial_iter', 'repr_as_obj', 'Reusable',
-    'roundrobin', 'shared_call', 'Si', 'sizeof', 'sliced', 'stream_batched',
-    'summary', 'threadlocal', 'time_this', 'timer', 'trace_module', 'trace',
-    'windowed'
+    'mapped', 'memoize', 'memprof', 'repr_as_obj', 'Reusable', 'roundrobin',
+    'shared_call', 'Si', 'sizeof', 'sliced', 'stream_batched', 'summary',
+    'threadlocal', 'time_this', 'timer', 'trace_module', 'trace', 'windowed'
 ]
 
 _patch_print.apply()
