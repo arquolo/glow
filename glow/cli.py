4b9e8ee1d1248298afa83a21d14c416728ccf6ed
Paul M <arquolo@gmail.com>
2021-04-01 20:08:30 +0500

    Fix CLI - add compat for Py3.8

--- glow/cli.py
+++ glow/cli.py
@@ -3,11 +3,13 @@ from __future__ import annotations  # until 3.10
 __all__ = ['arg', 'parse_args']
 
 import sys
-from argparse import (ArgumentDefaultsHelpFormatter, ArgumentParser,
-                      BooleanOptionalAction)
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from collections.abc import Sequence
 from dataclasses import MISSING, field, is_dataclass
 from typing import TypeVar, Union, get_args, get_origin, get_type_hints
+
+if sys.version_info >= (3, 9):
+    from argparse import BooleanOptionalAction
 
 _T = TypeVar('_T')
 
@@ -59,11 +61,18 @@ def _parse(
         if type_ is bool:
             if default is MISSING:
                 raise ValueError(f'Boolean field "{name}" must have default')
-            parser.add_argument(
-                f'--{snake}',
-                action=BooleanOptionalAction,
-                default=default,
-                help=help_)
+            if sys.version_info >= (3, 9):
+                parser.add_argument(
+                    f'--{snake}',
+                    action=BooleanOptionalAction, default=default, help=help_)
+            else:
+                if default:
+                    parser.add_argument(
+                        f'--no-{snake}',
+                        action='store_false', dest=snake, help=help_)
+                else:
+                    parser.add_argument(
+                        f'--{snake}', action='store_true', help=help_)
         elif default is not MISSING:
             origin = get_origin(type_)
             args = get_args(type_)
