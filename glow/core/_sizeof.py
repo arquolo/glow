a597b9a8e9fcfc3eebfcd59e86d3143a0313fcb0
Paul M <arquolo@gmail.com>
2021-05-28 16:23:11 +0500

    Refactor sizeof to work with more types

--- glow/core/_sizeof.py
+++ glow/core/_sizeof.py
@@ -2,79 +2,124 @@ from __future__ import annotations  # until 3.10
 
 __all__ = ['sizeof']
 
+import ctypes
 import functools
 import sys
 from collections import abc
+from ctypes import pythonapi
 from enum import Enum
 from inspect import isgetsetdescriptor, ismemberdescriptor
+from numbers import Number
+from types import FunctionType, ModuleType
 
 import numpy as np
 
 from ._import_hook import when_imported
-from ._repr import Si
+from ._repr import si_bin
+
+_ZERO_DEPTH_BASES = (str, bytes, bytearray, range, Number, Enum)
+_SINGLETONS = (type, bool, FunctionType, ModuleType)
+
+pythonapi._PyObject_GetDictPtr.argtypes = (ctypes.py_object, )
+pythonapi._PyObject_GetDictPtr.restype = ctypes.POINTER(ctypes.py_object)
 
 
-def for_unseen(fn, default=Si.bits):
+def unique_only(fn):
     """Protection from self-referencing"""
-    def wrapper(obj, seen: set[int] = None) -> Si:
-        if seen is None:
-            seen = set()
-        id_ = id(obj)
-        if id_ in seen:
-            return default()
-
-        seen.add(id_)
-        return fn(obj, seen)
+    def wrapper(obj, seen: set[int]) -> int:
+        if (id_ := id(obj)) not in seen:
+            seen.add(id_)
+            return fn(obj, seen)
+        return 0
 
     return functools.update_wrapper(wrapper, fn)
 
 
+def true_vars(obj) -> dict[str, object] | None:
+    """Get instance's vars(), even it's hidden.
+
+    Inspired by [magic_get_dict](https://stackoverflow.com/a/45315745/9868257)
+    """
+    tp = type(obj)
+
+    if sys.implementation.name == 'cpython':
+        if ptr := pythonapi._PyObject_GetDictPtr(obj):
+            return ptr.contents.value
+        return None
+
+        # if offset := tp.__dictoffset__:
+        #     if offset < 0:
+        #         offset += tp.__sizeof__(obj)  # type: ignore
+        #     addr = id(obj) + offset
+        #     ptr = ctypes.cast(addr, ctypes.POINTER(ctypes.py_object))
+        #     return ptr.contents.value
+        # return None
+
+    if hasattr(obj, '__dict__'):
+        for cls in tp.__mro__:
+            if (attr := vars(cls).get('__dict__')) is not None:
+                return (vars(obj) if (isgetsetdescriptor(attr) or
+                                      ismemberdescriptor(attr)) else None)
+    return None
+
+
 @functools.singledispatch
-@for_unseen
-def sizeof(obj, seen: set[int] = None) -> Si:
+@unique_only
+def _sizeof(obj, seen: set[int]) -> int:
+    if obj is None:
+        return 0
+
+    tp = type(obj)  # Real type, may differ from obj.__class__
+    if issubclass(tp, _SINGLETONS):
+        return 0
+
+    size = sys.getsizeof(obj)
+    if issubclass(tp, _ZERO_DEPTH_BASES):
+        return size
+
+    size += _sizeof(true_vars(obj), seen)
+
+    if hasattr(obj, '__wrapped__'):  # Unwrap
+        size += _sizeof(obj.__wrapped__, seen)
+        return size
+
+    if issubclass(tp, dict):
+        size += sum(_sizeof(k, seen) for k in obj.keys())
+        size += sum(_sizeof(v, seen) for v in obj.values())
+    elif issubclass(tp, abc.Collection):
+        size += sum(_sizeof(item, seen) for item in obj)
+
+    if hasattr(obj, '__slots__'):
+        size += sum(
+            _sizeof(getattr(obj, slot, None), seen)
+            for cls in tp.__mro__ if (slots := getattr(cls, '__slots__', ()))
+            for slot in ([slots] if isinstance(slots, str) else slots))
+
+    return size
+
+
+@unique_only
+def _sizeof_numpy(obj, seen: set[int]) -> int:
+    return _sizeof(obj.base, seen) + sys.getsizeof(obj)
+
+
+@unique_only
+def _sizeof_torch(obj, _: set[int]) -> int:
+    size = sys.getsizeof(obj)
+    if not obj.is_cuda:
+        size += obj.numel() * obj.element_size()
+    return size  # TODO: fix if useless when grads are attached
+
+
+_sizeof.register(np.ndarray, _sizeof_numpy)
+when_imported('torch')(
+    lambda torch: _sizeof.register(torch.Tensor, _sizeof_torch))
+
+
+def sizeof(obj) -> int:
     """Computes size of object, no matter how complex it is.
 
     Inspired by
     [PySize](https://github.com/bosswissam/pysize/blob/master/pysize.py)
     """
-    size = sys.getsizeof(obj)
-    if isinstance(obj, (str, bytes, bytearray, Si, Enum)):
-        return Si.bits(size)
-
-    if hasattr(obj, '__dict__'):
-        for d in (vars(cl)['__dict__']
-                  for cl in obj.__class__.__mro__ if '__dict__' in vars(cl)):
-            if isgetsetdescriptor(d) or ismemberdescriptor(d):
-                size += sizeof(vars(obj), seen)
-            break
-
-    if isinstance(obj, dict):
-        size += sum(sizeof(k, seen) for k in obj.keys())
-        size += sum(sizeof(v, seen) for v in obj.values())
-    elif isinstance(obj, abc.Collection):
-        size += sum(sizeof(item, seen) for item in obj)
-
-    if hasattr(obj, '__slots__'):
-        size += sum(
-            sizeof(getattr(obj, slot, None), seen)
-            for class_ in type(obj).mro()
-            for slot in getattr(class_, '__slots__', ()))
-    return Si.bits(size)
-
-
-@for_unseen
-def _sizeof_numpy(obj, _=None) -> Si:
-    return Si.bits(sys.getsizeof(obj if obj.base is None else obj.base))
-
-
-@for_unseen
-def _sizeof_torch(obj, _=None) -> Si:
-    size = sys.getsizeof(obj)
-    if not obj.is_cuda:
-        size += obj.numel() * obj.element_size()
-    return Si.bits(size)  # TODO: test, maybe useless when grads are attached
-
-
-sizeof.register(np.ndarray, _sizeof_numpy)
-when_imported('torch')(
-    lambda torch: sizeof.register(torch.Tensor, _sizeof_torch))
+    return si_bin(_sizeof(obj, set()))
