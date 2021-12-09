1130ebc31191649fba35f43c5a980b7fe1454bd3
Paul Maevskikh <arquolo@gmail.com>
2021-12-09 19:05:45 +0500

    Refactor scipy patch

--- glow/core/_patch_scipy.py
+++ glow/core/_patch_scipy.py
@@ -11,48 +11,56 @@ __all__ = ['apply']
 import ctypes
 import os
 import sys
+import warnings
+from collections.abc import Iterator
+from importlib.util import find_spec
 from pathlib import Path
 
 _FORTRAN_FLAG = 'FOR_DISABLE_CONSOLE_CTRL_HANDLER'
 
 
-def patch_handler() -> None:
+def _get_conda_libs() -> Iterator[ctypes.CDLL]:
+    """Preload DLLs from icc_rt conda package"""
     root = Path(sys.prefix)
-    if not (root / 'conda-meta').exists():
-        return
+    if (root / 'conda-meta').exists() and find_spec('scipy'):
+        for dllname in ('libmmd.dll', 'libifcoremd.dll'):
+            if (dll := root / f'Library/bin/{dllname}').exists():
+                yield ctypes.CDLL(dll.as_posix())
 
-    # Preload DLLs
-    for dllname in ('libmmd.dll', 'libifcoremd.dll'):
-        dllpath = root / 'Library' / 'bin' / dllname
-        if dllpath.exists():
-            ctypes.CDLL(dllpath.as_posix())
 
-    # Picked from (stackoverflow)[https://stackoverflow.com/a/39021051/9868257]
-
+def _patch_handler_and_load_scipy() -> None:
+    # Source - (stackoverflow)[https://stackoverflow.com/a/39021051/9868257]
     ptr = ctypes.c_void_p()
     ok = ctypes.windll.kernel32.VirtualProtect(
         ptr, ctypes.c_size_t(1), 0x40, ctypes.byref(ctypes.c_uint32(0)))
     if not ok or (addr := ptr.value) is None:
         return
-    code: bytearray = (ctypes.c_char * 3).from_address(addr)  # type: ignore
 
-    patch = b'\xC2\x08\x00' if ctypes.sizeof(ctypes.c_void_p) == 4 else b'\xC3'
-    # Meaningless if scipy.stats is loaded
-    if code and 'scipy.stats' not in sys.modules:
-        patch_size = len(patch)
-        old_code, code[:patch_size] = code[:patch_size], patch
-        try:
-            import scipy.stats  # noqa: F401
-        finally:
-            code[:patch_size] = old_code
+    code: bytearray = (ctypes.c_char * 3).from_address(addr)  # type: ignore
+    if not code:
+        return
+
+    new = b'\xC2\x08\x00' if ctypes.sizeof(ctypes.c_void_p) == 4 else b'\xC3'
+    patch_size = len(new)
+    old, code[:patch_size] = code[:patch_size], new
+    try:
+        import scipy.stats  # noqa: F401
+    finally:
+        code[:patch_size] = old
 
 
 def apply() -> None:
-    if sys.platform != 'win32' or _FORTRAN_FLAG in os.environ:
+    if (sys.platform != 'win32' or
+            _FORTRAN_FLAG in os.environ or  # Patch is already applied
+            not [*_get_conda_libs()]):  # Onle Anaconda's SciPy is affected
         return
-    # Add flag to environment, child processes will inherit it
-    os.environ[_FORTRAN_FLAG] = '1'
-    try:
-        patch_handler()
-    except BaseException:  # noqa: PIE786
-        pass
+
+    os.environ[_FORTRAN_FLAG] = '1'  # Child will inherit this, and work fine
+
+    warnings.warn('Ctrl-C on Windows is broken when scipy is from conda. '
+                  'Please use scipy from PyPI')
+    if 'scipy.stats' in sys.modules:
+        warnings.warn('Cannot fix handling of Ctrl-C in current process. '
+                      'Import glow before scipy.stats to fix this.')
+    else:
+        _patch_handler_and_load_scipy()
