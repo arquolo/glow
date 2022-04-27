02f43cf99bd466418584378b83e4946a230cd9e3
Paul Maevskikh <arquolo@gmail.com>
2022-04-28 01:00:23 +0500

    Refactor wrap.concurency

--- glow/core/wrap/concurrency.py
+++ glow/core/wrap/concurrency.py
@@ -1,19 +1,26 @@
+from __future__ import annotations
+
 __all__ = ['stream_batched', 'call_once', 'threadlocal', 'shared_call']
 
-import functools
 import threading
-import time
-from collections.abc import Callable, Sequence
+from collections.abc import Callable, Hashable, Sequence
 from concurrent.futures import Future
-from contextlib import ExitStack
+from dataclasses import dataclass, field
+from functools import partial, update_wrapper
 from queue import Empty, SimpleQueue
-from threading import Thread
-from typing import Any, TypeVar, cast
+from threading import Lock, Thread
+from time import monotonic, sleep
+from typing import Any, NoReturn, TypeVar, cast
 from weakref import WeakValueDictionary
+
+from .util import make_key
 
 _T = TypeVar('_T')
 _F = TypeVar('_F', bound=Callable)
+_Make = Callable[[], _T]
 _ZeroArgsF = TypeVar('_ZeroArgsF', bound=Callable[[], Any])
+
+_unset = object()
 
 
 def threadlocal(fn: Callable[..., _T], *args: object,
@@ -28,68 +35,65 @@ def threadlocal(fn: Callable[..., _T], *args: object,
             local_.obj = fn(*args, **kwargs)
             return local_.obj
 
-    return wrapper
+    return update_wrapper(wrapper, fn)
 
 
-class _DeferredStack(ExitStack):
-    """
-    ExitStack that allows deferring.
-    When return value of callback function should be accessible, use this.
-    """
-    def defer(self, fn: Callable[..., _T], *args, **kwargs) -> Future[_T]:
-        future: Future[_T] = Future()
+@dataclass
+class _UniqueTask:
+    _fn: Callable[[], Any]
+    _lock: Lock = field(default_factory=Lock)
+    _result: Any = _unset
+    _exception: BaseException | None = None
 
-        def apply(future: Future[_T]) -> None:
+    def run(self):
+        with self._lock:
+            if self._exception:
+                raise self._exception
+            if self._result is not _unset:
+                return self._result
+
             try:
-                result = fn(*args, **kwargs)
-            except BaseException as exc:  # noqa: PIE786
-                future.set_exception(exc)
+                r = self._fn()
+            except BaseException as e:
+                self._exception = e
+                raise
             else:
-                future.set_result(result)
-
-        self.callback(apply, future)
-        return future
+                self._result = r
+                return r
 
 
 def call_once(fn: _ZeroArgsF) -> _ZeroArgsF:
-    """Makes `fn()` callable a singleton"""
-    lock = threading.RLock()
-
+    """Makes `fn()` callable a singleton.
+    DO NOT USE with recursive functions"""
     def wrapper():
-        with _DeferredStack() as stack, lock:
-            if fn._future is None:
-                # This way setting future is protected, but fn() is not
-                fn._future = stack.defer(fn)
+        return task.run()
 
-        return fn._future.result()
-
-    fn._future = None  # type: ignore
-    return cast(_ZeroArgsF, functools.update_wrapper(wrapper, fn))
+    fn._task = task = _UniqueTask(fn)  # type: ignore
+    return cast(_ZeroArgsF, update_wrapper(wrapper, fn))
 
 
 def shared_call(fn: _F) -> _F:
-    """Merges concurrent calls to `fn` with the same `args` to single one"""
-    lock = threading.RLock()
-    futures: WeakValueDictionary[str, Future] = WeakValueDictionary()
+    """Merges concurrent calls to `fn` with the same `args` to single one.
+    DO NOT USE with recursive functions"""
+    tasks: WeakValueDictionary[Hashable, _UniqueTask] = WeakValueDictionary()
+    lock = Lock()
 
     def wrapper(*args, **kwargs):
-        key = f'{fn}{args}{kwargs}'
+        key = make_key(*args, **kwargs)
 
-        with _DeferredStack() as stack, lock:
-            try:
-                future = futures[key]
-            except KeyError:
-                futures[key] = future = stack.defer(fn, *args, **kwargs)
+        with lock:  # Create only one task per args-kwargs set
+            if not (task := tasks.get(key)):
+                tasks[key] = task = _UniqueTask(partial(fn, *args, **kwargs))
 
-        return future.result()
+        return task.run()
 
-    return cast(_F, functools.update_wrapper(wrapper, fn))
+    return cast(_F, update_wrapper(wrapper, fn))
 
 
-def _batch_apply(func: Callable, args: Sequence, futures: Sequence[Future]):
+def _batch_invoke(func: _Make[Sequence[_T]], futures: Sequence[Future[_T]]):
     try:
-        results = func(args)
-        assert len(args) == len(results)
+        results = func()
+        assert len(results) == len(futures)
     except BaseException as exc:  # noqa: PIE786
         for fut in futures:
             fut.set_exception(exc)
@@ -108,7 +112,7 @@ def stream_batched(func=None, *, batch_size, latency=0.1, timeout=20.):
     Simplified version of https://github.com/ShannonAI/service-streamer
     """
     if func is None:
-        return functools.partial(
+        return partial(
             stream_batched,
             batch_size=batch_size,
             latency=latency,
@@ -118,25 +122,26 @@ def stream_batched(func=None, *, batch_size, latency=0.1, timeout=20.):
     buf = SimpleQueue()
 
     def _fetch_batch():
-        end_time = time.monotonic() + latency
+        end_time = monotonic() + latency
         for _ in range(batch_size):
             try:
-                yield buf.get(timeout=end_time - time.monotonic())
+                yield buf.get(timeout=end_time - monotonic())
             except (Empty, ValueError):  # ValueError on negative timeout
                 return
 
-    def _serve_forever():
+    def _serve_forever() -> NoReturn:
         while True:
             if batch := [*_fetch_batch()]:
-                _batch_apply(func, *zip(*batch))
+                fs, items = zip(*batch)
+                _batch_invoke(partial(func, items), fs)
             else:
-                time.sleep(0.001)
+                sleep(0.001)
 
-    def wrapper(batch):
-        futures = [Future() for _ in batch]
-        for item, fut in zip(batch, futures):
-            buf.put((item, fut))
-        return [f.result(timeout=timeout) for f in futures]
+    def wrapper(items):
+        fs_iter = iter(Future, None)
+        fs = [f for x, f in zip(items, fs_iter) if not buf.put((f, x))]
+        end_time = monotonic() + timeout
+        return [f.result(end_time - monotonic()) for f in fs]
 
     Thread(target=_serve_forever, daemon=True).start()
-    return functools.update_wrapper(wrapper, func)
+    return update_wrapper(wrapper, func)
