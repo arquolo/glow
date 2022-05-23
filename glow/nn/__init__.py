27bdd41b1b3b6a6c82919a276dea78e37fb575c2
Paul Maevskikh <arquolo@gmail.com>
2022-05-23 15:08:03 +0500

    Make import of nn.plot_model safe

--- glow/nn/__init__.py
+++ glow/nn/__init__.py
@@ -1,3 +1,6 @@
+from importlib import import_module
+from typing import TYPE_CHECKING
+
 from ._loader import make_loader
 from ._stepper import Stepper
 from .amp import get_amp_context
@@ -5,7 +8,6 @@ from .driver import get_gpu_state
 from .modules import (Dropsample, Ensemble, Noise, SplitAttention,
                       SqueezeExcitation, Upscale2d)
 from .optimizers import SGDW, AdamW, RAdam
-from .plot import plot_model
 from .util import device, dump_to_onnx, frozen, inference, param_count, profile
 
 __all__ = [
@@ -14,3 +16,24 @@ __all__ = [
     'dump_to_onnx', 'frozen', 'get_amp_context', 'get_gpu_state', 'inference',
     'make_loader', 'param_count', 'plot_model', 'profile'
 ]
+
+_exports = {
+    '.plot': ['plot_model'],
+}
+_submodule_by_name = {
+    name: modname for modname, names in _exports.items() for name in names
+}
+
+if TYPE_CHECKING:
+    from .plot import plot_model
+else:
+
+    def __getattr__(name: str):
+        if modname := _submodule_by_name.get(name):
+            mod = import_module(modname, __package__)
+            globals()[name] = obj = getattr(mod, name)
+            return obj
+        raise AttributeError(f'No attribute {name}')
+
+    def __dir__():
+        return __all__
