2cfd847f7395fb4e11388c592c767b6810df24a0
Paul M <arquolo@gmail.com>
2023-10-13 00:38:37 +0500

    DROP itertools `len` - py3.12

--- src/glow/_patch_len.py
+++ src/glow/_patch_len.py
@@ -1,61 +1,44 @@
-"""Make builtin iterators compatible with `len`"""
+"""
+Makes builtin iterators compatible with `len`.
+
+Supports:
+- str, bytes, range, tuple, list, set, deque iterators
+- dict keys/values/items iterators
+- OrderedDict keys/values/items iterators
+- reversed
+- map
+- zip
+
+Since Python 3.12 `itertools` doesn't support serialization,
+thus `len` will never work for them.
+"""
 __all__ = ['apply']
 
 import builtins
 import functools
-import math
 import operator
-from collections.abc import Iterable, Iterator, Sized
-from dataclasses import dataclass
-from itertools import (accumulate, count, cycle, islice, product, repeat,
-                       starmap, tee, zip_longest)
-from typing import Generic, Protocol, TypeVar, runtime_checkable
-
-# -------------------------- iterable-size proxies ---------------------------
-
-_T_co = TypeVar('_T_co', covariant=True)
-_INF = float('inf')
-
-
-@runtime_checkable
-class SizedIterable(Sized, Iterable[_T_co], Protocol[_T_co]):
-    ...
-
-
-@dataclass(repr=False, frozen=True)
-class _SizedIterable(Generic[_T_co]):
-    _it: Iterable[_T_co]
-    _size: int
-
-    def __iter__(self) -> Iterator[_T_co]:
-        return iter(self._it)
-
-    def __len__(self) -> int:
-        return self._size
-
-    def __repr__(self) -> str:
-        line = object.__repr__(self._it)
-        line = line.removeprefix('<').removesuffix('>')
-        return f'<{line} with {self._size} items>'
-
+from collections import OrderedDict, deque
+from collections.abc import Iterable
 
 # --------------------------------- builtins ---------------------------------
 
 len_hint = functools.singledispatch(builtins.len)
 
-_tee: type = tee(())[0].__class__
 _iterables: list[Iterable] = [
     '', b'',
     range(0), (), [], {}, {}.keys(), {}.values(), {}.items(),
     reversed(()),
     reversed([]),
     set(),
-    frozenset()
+    frozenset(),
+    deque()
 ]
 _transparent_types: tuple[type, ...] = tuple(
     it.__iter__().__class__ for it in _iterables)
 for _tp in _transparent_types:
     len_hint.register(_tp, operator.length_hint)
+
+_odict_iter_tp: type = OrderedDict().__iter__().__class__
 
 
 def _are_definitely_independent(iters):
@@ -93,96 +76,10 @@ def _len_map(x):  # type: ignore[misc]
     raise TypeError
 
 
-# --------------------------- itertools.infinite ---------------------------
-
-
-@len_hint.register(count)
-def _len_count(_):  # type: ignore[misc]
-    return _INF
-
-
-@len_hint.register(cycle)
-def _len_cycle(x):  # type: ignore[misc]
-    _, [iterable], (buf, pos) = x.__reduce__()
-    if buf or len(iterable):
-        return _INF
-    return 0
-
-
-@len_hint.register(repeat)
-def _len_repeat(x):  # type: ignore[misc]
-    _, (obj, *left) = x.__reduce__()
-    return left[0] if left else _INF
-
-
-# ---------------------------- itertools.finite ----------------------------
-
-
-@len_hint.register(accumulate)
-def _len_accumulate(x):  # type: ignore[misc]
-    _, (seq, _fn), _total = x.__reduce__()
-    return len(seq)
-
-
-# @len_hint.register(chain)
-
-
-@len_hint.register(islice)
-def _len_islice(x):  # type: ignore[misc]
-    _, (it, start, *stop_step), done = x.__reduce__()
-    if not stop_step:
-        return 0
-    stop, step = stop_step
-    total = len(it) + done
-    stop = total if stop is None else min(total, stop)
-    if math.isinf(stop):
-        # range can't handle inf
-        return _INF
-    return len(range(start, stop, step))
-
-
-@len_hint.register(starmap)
-def _len_starmap(x):  # type: ignore[misc]
-    _, (_fn, seq) = x.__reduce__()
-    return len(seq)
-
-
-@len_hint.register(_tee)
-def _len_tee(x):
-    _, [empty_tuple], (dataobject, pos) = x.__reduce__()
-    _, (src, buf, none) = dataobject.__reduce__()
-    return len(src) + len(buf) - pos
-
-
-@len_hint.register(zip_longest)
-def _len_zip_longest(x):  # type: ignore[misc]
-    _, iters, _pad = x.__reduce__()
-    if _are_definitely_independent(iters):
-        return max(map(len, iters))
-    raise TypeError
-
-
-# -------------------------- itertools.combinatoric -------------------------
-
-
-@len_hint.register(product)
-def _len_product(x):  # type: ignore[misc]
-    _, seqs, *pos = x.__reduce__()
-
-    # Greedy caches all input iterables, no need to check interference
-    lens = *map(len, seqs),
-    total = functools.reduce(operator.mul, lens, 1)
-    if not pos:
-        return total
-
-    strides = accumulate((1, ) + lens[:0:-1], operator.mul)
-    offset = sum(map(operator.mul, strides, reversed(pos[0])))
-    return total - offset - 1
-
-
-# @len_hint.register(permutations)
-# @len_hint.register(combinations)
-# @len_hint.register(combinations_with_replacement)
+@len_hint.register(_odict_iter_tp)
+def _len_odict_iter(x):
+    _, (items, ) = x.__reduce__()
+    return len(items)
 
 
 def apply():
