71d0e48c70447dad859402791b89cfb3f0def00b
Paul M <arquolo@gmail.com>
2024-04-26 01:07:16 +0500

    MAINT apack/aceil/afloor/around

--- src/glow/_array.py
+++ src/glow/_array.py
@@ -1,16 +1,15 @@
 __all__ = ['aceil', 'afloor', 'apack', 'around', 'smallest_dtype']
 
-from typing import TypeVar
-
 import numpy as np
 import numpy.typing as npt
 
-_Scalar_co = TypeVar('_Scalar_co', bound=np.number, covariant=True)
-
 
 def _force_int(op: np.ufunc, a: npt.NDArray[np.number],
-               dtype: np.dtype[_Scalar_co]) -> npt.NDArray[_Scalar_co]:
-    assert dtype.kind in 'iu'
+               dtype: npt.DTypeLike) -> npt.NDArray[np.integer]:
+    dtype = np.dtype(dtype)
+    if dtype.kind not in 'iu':
+        raise ValueError(f'Cannot cast to non-integer dtype: {dtype}')
+
     match a.dtype.kind:
         case 'b' | 'u' | 'i':
             return a.astype(dtype)
@@ -23,19 +22,19 @@ def _force_int(op: np.ufunc, a: npt.NDArray[np.number],
 def around(x: npt.NDArray[np.number],
            dtype: npt.DTypeLike = int) -> npt.NDArray[np.integer]:
     """Faster alternative to `np.round(x).astype(int)`"""
-    return _force_int(np.rint, x, np.dtype(dtype))
+    return _force_int(np.rint, x, dtype)
 
 
 def aceil(x: npt.NDArray[np.number],
           dtype: npt.DTypeLike = int) -> npt.NDArray[np.integer]:
     """Faster alternative to `np.ceil(x).astype(int)`"""
-    return _force_int(np.ceil, x, np.dtype(dtype))
+    return _force_int(np.ceil, x, dtype)
 
 
 def afloor(x: npt.NDArray[np.number],
            dtype: npt.DTypeLike = int) -> npt.NDArray[np.integer]:
     """Faster alternative to `np.floor(x).astype(int)`"""
-    return _force_int(np.floor, x, np.dtype(dtype))
+    return _force_int(np.floor, x, dtype)
 
 
 def smallest_dtype(a_min: float | int,
@@ -44,17 +43,23 @@ def smallest_dtype(a_min: float | int,
     return r if a_max is None else np.result_type(r, a_max)
 
 
-def apack(a: npt.ArrayLike,
+def apack(a: npt.ArrayLike | npt.NDArray[np.integer],
           a_min: int | None = None,
           a_max: int | None = None) -> npt.NDArray[np.integer]:
-    """Convert array to smallest dtype"""
+    """Convert integer array to smallest dtype"""
     a = np.asarray(a)
+    if a.dtype.kind != 'i':
+        raise ValueError(f'Cannot pack non-integer array: {a.dtype}')
     if not a.size:
         return a
+
     if a_min is None:
         a_min = a.min()
+        assert a_min is not None
+
     if a_max is None:
         a_max = a.max()
+
     if (dtype := smallest_dtype(a_min, a_max)).itemsize < a.itemsize:
         return a.astype(dtype)
     return a
