c83c371d3c8b918c5777700bb7b91282885123d2
Paul M <arquolo@gmail.com>
2025-02-03 22:03:35 +0500

    LINT

--- src/glow/_async.py
+++ src/glow/_async.py
@@ -7,57 +7,61 @@ from collections.abc import (
     AsyncIterable,
     AsyncIterator,
     Callable,
+    Collection,
     Coroutine,
     Iterable,
+    Iterator,
 )
-from itertools import starmap
 from typing import Any, TypeGuard
 
-type _Coro[T] = Coroutine[Any, Any, T]
+type _AnyIterable[T] = AsyncIterable[T] | Iterable[T]
+type _AnyIterator[T] = AsyncIterator[T] | Iterator[T]
 
 
 def amap[
     R
 ](
-    func: Callable[..., _Coro[R]],
+    func: Callable[..., Coroutine[Any, Any, R]],
     /,
-    *iterables: Iterable | AsyncIterable,
+    *iterables: _AnyIterable,
     limit: int,
     unordered: bool = False,
 ) -> AsyncIterator[R]:
-    return astarmap(func, azip(*iterables), limit=limit, unordered=unordered)
+    it = zip(*iterables) if _all_sync_iters(iterables) else azip(*iterables)
+    return astarmap(func, it, limit=limit, unordered=unordered)
 
 
 async def astarmap[
     *Ts, R
 ](
-    func: Callable[[*Ts], _Coro[R]],
-    iterable: Iterable[tuple[*Ts]] | AsyncIterable[tuple[*Ts]],
+    func: Callable[[*Ts], Coroutine[Any, Any, R]],
+    iterable: _AnyIterable[tuple[*Ts]],
     /,
     *,
     limit: int,
     unordered: bool = False,
 ) -> AsyncIterator[R]:
     assert callable(func)
-    if isinstance(iterable, Iterable):
-        aws = _wrapgen(starmap(func, iterable))
-    else:
-        aws = (func(*args) async for args in iterable)
-
     async with asyncio.TaskGroup() as tg:
-        ts = (tg.create_task(aw) async for aw in aws)
+        ts = (
+            (tg.create_task(func(*args)) for args in iterable)
+            if isinstance(iterable, Iterable)
+            else (tg.create_task(func(*args)) async for args in iterable)
+        )
+
         it = (
             _iter_results_unordered(ts, limit=limit)
             if unordered
             else _iter_results(ts, limit=limit)
         )
+
         async for x in it:
             yield x
 
 
 async def _iter_results_unordered[
     T
-](ts: AsyncIterator[Task[T]], limit: int) -> AsyncIterator[T]:
+](ts: _AnyIterator[Task[T]], limit: int) -> AsyncIterator[T]:
     """
     Runs exactly `limit` tasks simultaneously (less in the end of iteration).
     Order of results is arbitrary.
@@ -65,17 +69,27 @@ async def _iter_results_unordered[
     todo = set[Task[T]]()
     done = Queue[Task[T]]()
 
-    def _done_callback(t: Task[T]) -> None:
+    def _todo_to_done(t: Task[T]) -> None:
         todo.discard(t)
         done.put_nowait(t)
 
     while True:
         # Prefill task buffer
-        while (len(todo) + done.qsize() < limit) and (
-            t := await anext(ts, None)
+        while len(todo) + done.qsize() < limit and (
+            t := (
+                next(ts, None)
+                if isinstance(ts, Iterator)
+                else await anext(ts, None)
+            )
         ):
-            todo.add(t)
-            t.add_done_callback(_done_callback)
+            # optimization: Immediately put to done if the task is
+            # already done (e.g. if the coro was able to complete eagerly),
+            # and skip scheduling a done callback
+            if t.done():
+                done.put_nowait(t)
+            else:
+                todo.add(t)
+                t.add_done_callback(_todo_to_done)
 
         # No more tasks to do and nothing more to schedule
         if not todo and done.empty():
@@ -91,7 +105,7 @@ async def _iter_results_unordered[
 
 async def _iter_results[
     T
-](ts: AsyncIterator[Task[T]], limit: int) -> AsyncIterator[T]:
+](ts: _AnyIterator[Task[T]], limit: int) -> AsyncIterator[T]:
     """
     Runs up to `limit` tasks simultaneously (less in the end of iteration).
     Order of results is preserved.
@@ -99,7 +113,13 @@ async def _iter_results[
     todo = deque[Task[T]]()
     while True:
         # Prefill task buffer
-        while len(todo) < limit and (t := await anext(ts, None)):
+        while len(todo) < limit and (
+            t := (
+                next(ts, None)
+                if isinstance(ts, Iterator)
+                else await anext(ts, None)
+            )
+        ):
             todo.append(t)
         if not todo:  # No more tasks to do and nothing more to schedule
             return
@@ -114,7 +134,7 @@ async def _iter_results[
             yield todo.popleft().result()
 
 
-async def azip(*iterables: Iterable | AsyncIterable) -> AsyncIterator[tuple]:
+async def azip(*iterables: _AnyIterable) -> AsyncIterator[tuple]:
     if _all_sync_iters(iterables):
         for x in zip(*iterables):
             yield x
@@ -134,8 +154,8 @@ async def azip(*iterables: Iterable | AsyncIterable) -> AsyncIterator[tuple]:
 
 
 def _all_sync_iters(
-    iterables: tuple[Iterable | AsyncIterable, ...]
-) -> TypeGuard[tuple[Iterable, ...]]:
+    iterables: Collection[_AnyIterable],
+) -> TypeGuard[Collection[Iterable]]:
     return all(isinstance(it, Iterable) for it in iterables)
 
 
