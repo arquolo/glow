24d10e98aef29a72e2a0de947f33f3dcf6c0d361
Paul M <arquolo@gmail.com>
2025-07-19 00:27:26 +0500

    MAINT profiler

--- src/glow/_profile.py
+++ src/glow/_profile.py
@@ -7,13 +7,12 @@ from contextlib import AbstractContextManager, contextmanager, nullcontext
 from dataclasses import dataclass, field
 from functools import partial
 from itertools import count
-from threading import get_ident
 from time import perf_counter_ns, process_time_ns, thread_time_ns
 from typing import TYPE_CHECKING
 
 from ._debug import whereami
 from ._repr import si, si_bin
-from ._streams import Stream, maximum_cumsum
+from ._streams import Stream, cumsum, maximum_cumsum
 from ._wrap import wrap
 
 if TYPE_CHECKING:
@@ -30,7 +29,7 @@ def memprof(
 ) -> Iterator[None]:
     global _THIS  # noqa: PLW0603
     if _THIS is None:
-        import psutil
+        import psutil  # noqa: PLC0415
 
         _THIS = psutil.Process()
 
@@ -97,61 +96,59 @@ def _to_fname(obj) -> str:
     return f'{obj.__module__}.{obj.__qualname__}'
 
 
-class _Times(dict[int, int]):
-    def add(self, value: int) -> None:
-        idx = get_ident()
-        self[idx] = self.get(idx, 0) + value
-
-    def total(self) -> int:
-        return sum(self.values())
-
-
 @dataclass(frozen=True, slots=True)
-class _Stat:
+class _Profiler:
+    # Call count
     calls: count = field(default_factory=count)
+    # Statistics requests count
     reads: count = field(default_factory=count)
+    # Parallelism/concurrency
     active_calls: Stream[int, int] = field(default_factory=maximum_cumsum)
-    busy_ns: _Times = field(default_factory=_Times)
-    idle_ns: _Times = field(default_factory=_Times)
+    # Time spent executing this thread, including kernel time, but not I/O.
+    busy_ns: Stream[int, int] = field(default_factory=cumsum)
+    # Idle time - elapsed I/O time (like time.sleep, lock.acquire, e.t.c.).
+    idle_ns: Stream[int, int] = field(default_factory=cumsum)
+
+    def suspend(self) -> Callable[[], None]:
+        self.idle_ns.send(-perf_counter_ns())
+        return self.resume
+
+    def resume(self) -> None:
+        self.idle_ns.send(+perf_counter_ns())
 
     def __call__[**P, R](
         self, op: Callable[P, R], *args: P.args, **kwargs: P.kwargs
     ) -> R:
         self.active_calls.send(+1)
-        # Real time (wall-clock time) - total elapsed time including
-        # waiting for I/O (like time.sleep, lock.acquire, e.t.c.).
-        total = perf_counter_ns()
-        # Time spent executing this thread, including kernel time.
-        active = thread_time_ns()
+        t_cpu = thread_time_ns()
+        self.idle_ns.send(+t_cpu - perf_counter_ns())
+        self.busy_ns.send(-t_cpu)
         try:
             return op(*args, **kwargs)
         finally:
-            active = thread_time_ns() - active
-            total = perf_counter_ns() - total
-            idle = max(0, total - active)
-            self.busy_ns.add(active)
-            self.idle_ns.add(idle)
+            t_cpu = thread_time_ns()
+            self.busy_ns.send(+t_cpu)
+            self.idle_ns.send(-t_cpu + perf_counter_ns())
             self.active_calls.send(-1)
 
     def stat(self) -> tuple[float, float, str] | None:
         if not (n := next(self.calls) - next(self.reads)):
             return None
-        w = self.active_calls.send(0)
-        t_ns = self.busy_ns.total()  # CPU = D(thread_time)
-        i_ns = self.idle_ns.total()  # idle = D(perf_counter) - D(thread_time)
-        t, i = t_ns / 1e9, i_ns / 1e9
+        concurrency = self.active_calls.send(0)
+        idle = self.idle_ns.send(0) / 1e9  # Wall-clock - user+sys
+        busy = self.busy_ns.send(0) / 1e9  # user+system time
 
-        tail = (
-            (f'{n} x {si(t / n)}s' + (f' @ {w}T' if w > 1 else ''))
-            if n > 1
-            else ''
-        )
-        return t, i, tail
+        tail = ''
+        if n > 1:
+            tail += f'{n} x {si(busy / n)}s'
+            if concurrency > 1:
+                tail += f' @ {concurrency}T'
+        return busy, idle, tail
 
 
 # Wall time, i.e. sum of per-thread times, excluding sleep
 _start = process_time_ns()
-_stats = defaultdict[str, _Stat](_Stat)
+_profilers = defaultdict[str, _Profiler](_Profiler)
 
 
 @atexit.register
@@ -159,11 +156,11 @@ def _print_stats(*names: str) -> None:
     all_busy = (process_time_ns() - _start + 1) / 1e9
 
     stats: list[tuple[float, float, str, str]] = []  # (busy, idle, tail, name)
-    names = names or tuple(_stats)
+    names = names or tuple(_profilers)
     for name in names:
-        if not (stat := _stats.pop(name, None)):
+        if not (profiler := _profilers.pop(name, None)):
             continue
-        if not (lines := stat.stat()):
+        if not (lines := profiler.stat()):
             continue
         stats.append((*lines, name))
 
@@ -188,7 +185,7 @@ def time_this(fn=None, /, *, name: str | None = None, disable: bool = False):
         name = _to_fname(fn)
 
     time_this.finalizers[fn] = fn.log_timing = partial(_print_stats, name)
-    return wrap(fn, _stats[name])
+    return wrap(fn, _profilers[name])
 
 
 time_this.finalizers = {}
