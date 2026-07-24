f182805c8940fc5cde67d61eaeab0a46763c7e08
Paul M <arquolo@gmail.com>
2026-07-24 21:04:57 +0500

    wip

--- src/glow/_concurrency.py
+++ src/glow/_concurrency.py
@@ -7,10 +7,14 @@ __all__ = [
 ]
 
 import threading
+import weakref
 from collections.abc import Callable, Generator, Sequence
 from concurrent.futures import Future, wait
 from functools import partial, update_wrapper
+from itertools import count
 from logging import getLogger
+from multiprocessing import Process
+from multiprocessing import Queue as MpQueue
 from queue import Empty, SimpleQueue
 from threading import Lock, Thread
 from time import monotonic, sleep
@@ -30,7 +34,8 @@ from ._futures import (
     get_usable_size,
 )
 from ._locking import q_get
-from ._types import Get
+from ._parallel import max_cpu_count
+from ._types import Get, Maybe, Some
 
 _LOGGER = getLogger(__name__)
 
@@ -252,3 +257,196 @@ def streaming[T, R](
     # TODO:  not yet bound method and plain function
     # TODO:  maybe implement __get__ on wrapper
     return update_wrapper(wrapper, func)
+
+
+def streaming2[T, R](
+    func: BatchFn[T, R] | None = None,
+    /,
+    *,
+    batch_size: int,
+    timeout: float = 0.1,
+    workers: int = 1,
+    pool_timeout: float = 20.0,
+) -> BatchDecorator | BatchFn[T, R]:
+    if func is None:
+        deco = partial(
+            streaming2,
+            batch_size=batch_size,
+            timeout=timeout,
+            workers=workers,
+            pool_timeout=pool_timeout,
+        )
+        return cast('BatchDecorator', deco)
+
+    assert callable(func)
+    assert workers >= 1
+
+    from ._thread_quota import ThreadQuota  # noqa: PLC0415
+
+    ex = ThreadQuota(workers)
+    futs: list[Future[list[R]]] = [Future()]
+    lock = Lock()
+    batch: list[T] = []
+    deadline = monotonic()
+
+    def _schedule_batch() -> None:
+        [fut] = futs
+        if batch:
+            ex.submit_f(fut, func, batch[:])
+            batch.clear()
+            futs[:] = [Future()]
+
+    def sync_late_submit() -> None:
+        with lock:
+            old_deadline = deadline
+            dt = monotonic() - old_deadline
+
+        if dt > 0:
+            sleep(dt)
+
+        with lock:
+            if deadline is not old_deadline:  # deadline moved forward
+                return
+            _schedule_batch()
+
+    def sync_submit(x: T) -> tuple[Future[list[R]], int]:
+        nonlocal deadline
+        with lock:
+            if not batch:
+                deadline = monotonic() + timeout
+                ex.submit(sync_late_submit)
+
+            [fut] = futs
+            idx = len(batch)
+
+            batch.append(x)
+            if len(batch) == batch_size or monotonic() >= deadline:
+                # FIXME: cancel sync_late_submit (how?)
+                _schedule_batch()
+
+            return fut, idx
+
+    def wrapper(xs: Sequence[T]) -> list[R]:
+        pairs = [sync_submit(x) for x in xs]
+
+        fs = {f for f, _ in pairs}
+        try:
+            dnd = wait(fs, pool_timeout, return_when='FIRST_EXCEPTION')
+        finally:  # Cancel all not-yet-running tasks, we're beyond deadline
+            for f in fs:
+                f.cancel()
+        if dnd.not_done:  # Some tasks timed out
+            del dnd, fs  # ? Break reference cycle
+            raise TimeoutError
+
+        rs, err = gather_fs(zip(fs, fs))
+        if err is None:
+            return [rs[f][i] for f, i in pairs]
+        with hide_frame:
+            raise err
+
+    return update_wrapper(wrapper, func)
+
+
+class Remote[**P, R]:
+    def __init__(
+        self,
+        func: Callable[P, R],
+        *,
+        num_workers: int | None = None,
+        mp: bool = False,
+        chunk_size: int = 1,
+        latency: float = 0.1,
+    ) -> None:
+        num_workers = num_workers or max_cpu_count(mp=mp)
+
+        self._ids = count()
+        self._worker = _RemoteWorker(func, num_workers + 1)
+        self._waiters: dict[int, Lock] = {}
+        self._results: dict[int, list[Maybe[R]] | BaseException] = {}
+
+        procs = [
+            Process(target=self._worker.run, daemon=True)
+            for _ in range(num_workers)
+        ]
+        for p in procs:
+            p.start()
+
+        def shutdown(jobs: MpQueue, procs: Sequence[Process]) -> None:
+            for _ in procs:
+                jobs.put(None)
+            for p in procs:
+                p.join()
+
+        self.close = weakref.finalize(self, shutdown, self._worker.jobs, procs)
+
+        Thread(target=self._mover, daemon=True).start()
+        self._batch = streaming(
+            self._batch_impl, batch_size=chunk_size, timeout=latency
+        )
+
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        [ret] = self._batch([(args, kwargs)])
+        if isinstance(ret, Some):
+            return ret.x
+        raise ret
+
+    def _batch_impl(
+        self, akws: Sequence[tuple[tuple, dict]]
+    ) -> list[Maybe[R]]:
+        if not akws:
+            return []
+
+        idx = next(self._ids)
+        lk = Lock()
+        lk.acquire()
+        self._waiters[idx] = lk
+        try:
+            self._worker.jobs.put((idx, list(akws)))  # IPC, push to worker
+        except BaseException:  # failed to serialize
+            lk.release()
+            self._waiters.pop(idx)
+            raise
+
+        with lk:  # wait till owned job is resolved
+            rets = self._results.pop(idx)
+            if isinstance(rets, BaseException):
+                raise rets
+            return rets
+
+    def _mover(self) -> Never:
+        while True:
+            idx, rets = self._worker.results.get()  # IPC, pull from worker
+            self._results[idx] = rets
+            if lk := self._waiters.pop(idx, None):  # notify waiter
+                lk.release()
+
+
+class _RemoteWorker[**P, R]:
+    jobs: MpQueue[tuple[int, list[tuple[tuple, dict]]] | None]
+    results: MpQueue[tuple[int, list[Maybe[R]] | BaseException]]
+
+    def __init__(self, func: Callable[P, R], qsize: int) -> None:
+        self.func = func
+        self.jobs = MpQueue(qsize)
+        self.results = MpQueue(qsize)
+
+    def run(self) -> None:
+        while True:
+            ijobs = self.jobs.get()
+            if not ijobs:
+                return
+            idx, jobs = ijobs
+            results: list[Maybe[R]] = []
+            for args, kwargs in jobs:
+                try:
+                    ret = self.func(*args, **kwargs)
+                except BaseException as exc:  # noqa: BLE001
+                    results.append(exc)
+                else:
+                    results.append(Some(ret))
+
+            try:
+                self.results.put((idx, results))
+            except BaseException as exc:  # noqa: BLE001
+                self.results.put((idx, exc))
