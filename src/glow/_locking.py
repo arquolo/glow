a4f0365d552184189c70dc11c769210744ab06a9
Paul M <arquolo@gmail.com>
2026-09-11 18:26:58 +0500

    MAINT locking - less acquire-release calls

--- src/glow/_locking.py
+++ src/glow/_locking.py
@@ -7,14 +7,25 @@ __all__ = [
 ]
 
 import sys
-from concurrent.futures import Future
+from concurrent.futures import CancelledError, Future
 from queue import Empty
 from time import sleep
-from typing import Protocol
+from typing import Protocol, cast
 
+from ._dev import hide_frame
 from ._types import Maybe
 
-_PERIOD = 0.01  # or sys.getswitchinterval() which is 0.005 on win32
+# On Windows lock.acquire called without a timeout is not interruptible
+# See issues
+# https://bugs.python.org/issue29971
+# https://github.com/dask/dask/pull/2144#issuecomment-290556996
+# https://github.com/dask/dask/pull/2144/files
+# https://github.com/python/cpython/issues/74157
+# FIXED in py3.15+
+if sys.platform == 'win32':
+    _SWITCH_INTERVAL = 0.01  # or sys.getswitchinterval() which is 0.005
+else:
+    _SWITCH_INTERVAL = None
 
 
 class AbsQueue[T](Protocol):
@@ -33,45 +44,54 @@ class AbsManager(Protocol):
 
 
 def maybe_future[T](f: Future[T], cancel: bool = False) -> Maybe[T]:
+    """Version with single acquire-release call of this:
+    >>> try:
+    ...     if (exc := f.exception()) is not None:  # <- acquire-release
+    ...         return exc
+    ...     return [f.result()]  # <- acquire-release
+    ... except:
+    ...     if cancel:
+    ...         f.cancel()  # <- acquire-release
+    ...     del f
+    ...     raise
+    """
+    invoke_callbacks = False
     try:
-        return exc if (exc := f_exception(f)) else [f.result()]
+        with f._condition, hide_frame:
+            try:
+                while f._state in ['PENDING', 'RUNNING']:
+                    with hide_frame:
+                        f._condition.wait(_SWITCH_INTERVAL)
+            except:
+                if cancel and f._state == 'PENDING':
+                    f._state = 'CANCELLED'
+                    f._condition.notify_all()
+                    invoke_callbacks = True
+                raise
+            else:
+                if f._state in ['CANCELLED', 'CANCELLED_AND_NOTIFIED']:
+                    raise CancelledError
+                if f._exception is not None:
+                    return f._exception
+                return [cast('T', f._result)]
+    except:
+        if invoke_callbacks:
+            f._invoke_callbacks()  # type: ignore[attr-defined]
+        raise
     finally:
-        if cancel:
-            f.cancel()
         del f
 
 
 if sys.platform == 'win32':
 
-    def f_exception[T](f: Future[T], /) -> BaseException | None:
-        # See issues
-        # https://bugs.python.org/issue29971
-        # https://github.com/dask/dask/pull/2144#issuecomment-290556996
-        # https://github.com/dask/dask/pull/2144/files
-        # https://github.com/python/cpython/issues/74157
-        # FIXED in py3.15+
-        while True:
-            try:
-                return f.exception(timeout=_PERIOD)
-            except TimeoutError:
-                sleep(0)  # Force switch to another thread to proceed
-
     def q_get[T](q: AbsQueue[T]) -> T:
-        # On Windows lock.acquire called without a timeout is not interruptible
-        # See issues
-        # https://bugs.python.org/issue29971
-        # https://github.com/dask/dask/pull/2144#issuecomment-290556996
-        # https://github.com/dask/dask/pull/2144/files
-        # https://github.com/python/cpython/issues/74157
-        # FIXED in py3.15+
         while True:
             try:
-                return q.get(timeout=_PERIOD)
+                return q.get(timeout=_SWITCH_INTERVAL)
             except Empty:
                 sleep(0)  # Force switch to another thread to proceed
 
 else:
-    f_exception = Future.exception
 
     def q_get[T](q: AbsQueue[T]) -> T:
         return q.get()
